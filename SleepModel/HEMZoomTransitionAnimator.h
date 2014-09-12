994f446a544cec17b7536c4059e8e185350a0cdf
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-09-12 15:15:46 -0700

    new zoom transition

--- SleepModel/HEMZoomTransitionAnimator.h
+++ SleepModel/HEMZoomTransitionAnimator.h
@@ -2,6 +2,11 @@
 //  HEMZoomTransitionAnimator.h
 //  Sense
 //
+//  This class provides a zoom like transition where the controller being presented
+//  will initially started zoomed in and will end in place while also fading in.
+//
+//  On dismissal, the controller presented will zoom "away" while also fading out
+//
 //  Created by Jimmy Lu on 9/12/14.
 //  Copyright (c) 2014 Hello, Inc. All rights reserved.
 //

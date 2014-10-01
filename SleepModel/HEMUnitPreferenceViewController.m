4e077b6336d62a28e43368afaea28620a80d7612
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-10-01 11:47:12 -0700

    updating units settings view controller

---
+++ SleepModel/HEMUnitPreferenceViewController.m
@@ -0,0 +1,125 @@
+//
+//  HEMUnitPreferenceViewController.m
+//  Sense
+//
+//  Created by Jimmy Lu on 10/1/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+#import <SenseKit/SENSettings.h>
+
+#import "HEMUnitPreferenceViewController.h"
+#import "HEMMainStoryboard.h"
+
+static CGFloat const kHemUnitPrefSegControlWidth = 157;
+
+@interface HEMUnitPreferenceViewController () <UITableViewDataSource, UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UITableView *unitTableView;
+
+@property (strong, nonatomic) IBOutlet UISegmentedControl* clockStyleSegmentControl;
+@property (strong, nonatomic) IBOutlet UISegmentedControl* temperatureSegmentControl;
+
+@end
+
+@implementation HEMUnitPreferenceViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [[self unitTableView] setTableFooterView:[[UIView alloc] init]];
+    [self setupClockControl];
+    [self setupTemperatureControl];
+}
+
+- (void)setupClockControl {
+    NSString* h24 = NSLocalizedString(@"settings.unit2.24-hour", nil);
+    NSString* h12 = NSLocalizedString(@"settings.unit2.12-hour", nil);
+    UISegmentedControl* clockControl = [[UISegmentedControl alloc] initWithItems:@[h24, h12]];
+    [clockControl setSelectedSegmentIndex:[SENSettings timeFormat] == SENTimeFormat24Hour?0:1];
+    [clockControl addTarget:self
+                     action:@selector(clockFormatChanged:)
+           forControlEvents:UIControlEventValueChanged];
+    
+    [self styleControl:clockControl];
+    [self setClockStyleSegmentControl:clockControl];
+}
+
+- (void)setupTemperatureControl {
+    NSString* celcius = NSLocalizedString(@"settings.unit2.celcius", nil);
+    NSString* fahrenheit = NSLocalizedString(@"settings.unit2.fahrenheit", nil);
+    NSInteger selectedIndex = [SENSettings temperatureFormat] == SENTemperatureFormatCentigrade?0:1;
+    
+    UISegmentedControl* tempControl = [[UISegmentedControl alloc] initWithItems:@[celcius, fahrenheit]];
+    [tempControl setSelectedSegmentIndex:selectedIndex];
+    [tempControl addTarget:self
+                     action:@selector(temperatureFormatChanged:)
+           forControlEvents:UIControlEventValueChanged];
+    
+    [self styleControl:tempControl];
+    [self setTemperatureSegmentControl:tempControl];
+}
+
+- (void)styleControl:(UISegmentedControl*)control {
+    [control setTintColor:[UIColor whiteColor]];
+    [control setTitleTextAttributes:@{
+                                NSFontAttributeName : [UIFont fontWithName:@"Agile-Light" size:18],
+                                NSForegroundColorAttributeName : [UIColor whiteColor]
+                           }
+                           forState:UIControlStateNormal];
+    
+    CGRect controlFrame = [control frame];
+    controlFrame.size.width = kHemUnitPrefSegControlWidth;
+    [control setFrame:controlFrame];
+}
+
+#pragma mark - Actions
+
+- (void)clockFormatChanged:(UISegmentedControl*)control {
+    if ([control selectedSegmentIndex] == 0) {
+        [SENSettings setTimeFormat:SENTimeFormat24Hour];
+    } else {
+        [SENSettings setTimeFormat:SENTimeFormat12Hour];
+    }
+}
+
+- (void)temperatureFormatChanged:(UISegmentedControl*)control {
+    if ([control selectedSegmentIndex] == 0) {
+        [SENSettings setTemperatureFormat:SENTemperatureFormatCentigrade];
+    } else {
+        [SENSettings setTemperatureFormat:SENTemperatureFormatFahrenheit];
+    }
+}
+
+#pragma mark - UITableViewDelegate / DataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 2;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView
+        cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    NSString* cellId = [HEMMainStoryboard unitCellReuseIdentifier];
+    return [tableView dequeueReusableCellWithIdentifier:cellId];
+}
+
+- (void)tableView:(UITableView *)tableView
+  willDisplayCell:(UITableViewCell *)cell
+forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if ([[cell reuseIdentifier] isEqualToString:[HEMMainStoryboard unitCellReuseIdentifier]]) {
+        UISegmentedControl* control = nil;
+        NSString* title = nil;
+        if ([indexPath row] == 0) {
+            title = NSLocalizedString(@"settings.units.clock", nil);
+            control = [self clockStyleSegmentControl];
+        } else if ([indexPath row] == 1) {
+            title = NSLocalizedString(@"settings.units.temp", nil);
+            control = [self temperatureSegmentControl];
+        }
+        
+        [[cell textLabel] setText:title];
+        [cell setAccessoryView:control];
+    }
+    
+}
+
+@end

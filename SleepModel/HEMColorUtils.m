4536128aea14af9420bd4e5c79ab76c0c87ed199
Delisa Mason <iskanamagus@gmail.com>
2014-10-02 16:34:12 -0700

    Update background gradient colors

--- SleepModel/HEMColorUtils.m
+++ SleepModel/HEMColorUtils.m
@@ -7,26 +7,30 @@
 + (void)configureLayer:(CAGradientLayer*)gradient
     withIntensityLevel:(CGFloat)level
 {
-    UIColor* topColor = [UIColor colorWithHue:0.658
-                                   saturation:MIN(MAX(level * 0.4, 0.15), 1)
+    UIColor* topColor = [UIColor colorWithHue:0.67
+                                   saturation:MIN(MAX(level * 0.8, 0.15), 0.8)
                                    brightness:MIN(MAX(level * 0.5, 0.25), 1)
                                         alpha:1.f];
-    UIColor* bottomColor = [UIColor colorWithHue:0.567
-                                      saturation:MIN(MAX(level * 0.5, 0.1), 1)
-                                      brightness:MIN(MAX(level * 1.5, 0.3), 1)
+    UIColor* middleColor = [UIColor colorWithHue:0.62
+                                      saturation:MIN(MAX(level * 0.6, 0.1), 1)
+                                      brightness:MIN(MAX(level * 0.8, 0.3), 1)
+                                           alpha:1.f];
+    UIColor* bottomColor = [UIColor colorWithHue:0.54
+                                      saturation:MIN(MAX(level * 0.3, 0.1), 1)
+                                      brightness:MIN(MAX(level * 1.1, 0.3), 1)
                                            alpha:1.f];
     gradient.colors = @[ (id)[topColor CGColor],
-                         (id)[bottomColor CGColor],
-                         (id)[topColor CGColor] ];
-    gradient.locations = @[ @0, @1, @1 ];
+                         (id)[middleColor CGColor],
+                         (id)[bottomColor CGColor] ];
+    gradient.locations = @[ @0, @(0.4), @1 ];
 }
 
 + (void)configureLayer:(CAGradientLayer*)gradient forHourOfDay:(NSInteger)hour {
     CGFloat intensity = 0;
-    if (hour < 12) {
-        intensity = hour / 11.f;
+    if (hour < 16) {
+        intensity = hour / 12.f;
     } else {
-        intensity = (23 - hour) / 12.f;
+        intensity = (23 - hour) / 8.f;
     }
     [self configureLayer:gradient withIntensityLevel:intensity];
 }

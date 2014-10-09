1f8d1c938fd931232ad80fc5c68dc76a5a0c41d2
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-10-09 15:09:12 -0700

    some pod stuff

--- Pods/SenseKit/Pod/Classes/Model/SENDevice.m
+++ Pods/SenseKit/Pod/Classes/Model/SENDevice.m
@@ -13,6 +13,8 @@
 @property (nonatomic, copy,   readwrite) NSString* deviceId;
 @property (nonatomic, assign, readwrite) SENDeviceType type;
 @property (nonatomic, assign, readwrite) SENDeviceState state;
+@property (nonatomic, copy,   readwrite) NSString* firmwareVersion;
+@property (nonatomic, strong, readwrite) NSDate* lastSeen;
 
 @end
 
@@ -20,12 +22,17 @@
 
 - (id)initWithDeviceId:(NSString*)deviceId
                   type:(SENDeviceType)type
-                 state:(SENDeviceState)state {
+                 state:(SENDeviceState)state
+       firmwareVersion:(NSString*)version
+              lastSeen:(NSDate*)lastSeen {
+    
     self = [super init];
     if (self) {
         [self setDeviceId:deviceId];
         [self setType:type];
         [self setState:state];
+        [self setFirmwareVersion:version];
+        [self setLastSeen:lastSeen];
     }
     return self;
 }

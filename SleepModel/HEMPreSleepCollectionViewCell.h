5a500c3dd621309780ec08c6033ef906bf64545d
Delisa Mason <iskanamagus@gmail.com>
2014-10-10 19:22:58 -0700

    Add presleep summary cell

---
+++ SleepModel/HEMPreSleepCollectionViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HEMPreSleepCollectionViewCell.h
+//  Sense
+//
+//  Created by Delisa Mason on 10/10/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HEMPreSleepCollectionViewCell : UICollectionViewCell
+
+@end
---
+++ SleepModel/HEMPreSleepCollectionViewCell.m
@@ -0,0 +1,17 @@
+//
+//  HEMPreSleepCollectionViewCell.m
+//  Sense
+//
+//  Created by Delisa Mason on 10/10/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+
+#import "HEMPreSleepCollectionViewCell.h"
+
+@implementation HEMPreSleepCollectionViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+@end

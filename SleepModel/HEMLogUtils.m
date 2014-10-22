0ec7b470a35ae9e2fb38e8e262e6ec2119da5733
Delisa Mason <iskanamagus@gmail.com>
2014-10-22 15:33:24 -0700

    Log device start info

--- SleepModel/HEMLogUtils.m
+++ SleepModel/HEMLogUtils.m
@@ -5,7 +5,7 @@
 //  Created by Delisa Mason on 10/22/14.
 //  Copyright (c) 2014 Hello, Inc. All rights reserved.
 //
-
+#import <sys/utsname.h>
 #import <CocoaLumberjack/DDLog.h>
 #import <CocoaLumberjack/DDFileLogger.h>
 #import <CocoaLumberjack/DDTTYLogger.h>
@@ -14,23 +14,42 @@
 @implementation HEMLogUtils
 
 static DDFileLogger* logUtilsFileLogger = nil;
+static CGFloat const HEMLogUtilsRollingFrequency = 60 * 60 * 24;
 
 + (void)enableLogger
 {
     logUtilsFileLogger = [[DDFileLogger alloc] init];
-    logUtilsFileLogger.rollingFrequency = 60 * 60 * 24;
+    logUtilsFileLogger.rollingFrequency = HEMLogUtilsRollingFrequency;
     logUtilsFileLogger.logFileManager.maximumNumberOfLogFiles = 7;
     [DDLog addLogger:logUtilsFileLogger];
 #if DEBUG
     [DDLog addLogger:[DDTTYLogger sharedInstance]];
-    DDLogVerbose(@"Added TTY logger");
 #endif
+    [self logSystemInfo];
 }
 
 + (NSData*)latestLogFileData
 {
     DDLogFileInfo* info = [logUtilsFileLogger.logFileManager.sortedLogFileInfos firstObject];
     return [NSData dataWithContentsOfFile:info.filePath];
+}
+
++ (void)logSystemInfo
+{
+    UIDevice* device = [UIDevice currentDevice];
+    NSString* appVersion = [[NSBundle mainBundle] objectForInfoDictionaryKey:(NSString *)kCFBundleVersionKey];
+    struct utsname systemInfo;
+    uname(&systemInfo);
+    NSString *modelName = [NSString stringWithCString:systemInfo.machine encoding:NSUTF8StringEncoding];
+
+    DDLogInfo(@"App started at %@", [NSDate date].description);
+    DDLogInfo(@"App Version: %@", appVersion);
+    DDLogInfo(@"iOS Version: %@", device.systemVersion);
+    DDLogInfo(@"Device: %@", modelName);
+    if (device.batteryLevel >= 0) {
+        NSString* batteryLevel = [NSString stringWithFormat:@"%0.f%%", device.batteryLevel * 100];
+        DDLogInfo(@"Battery Level: %@", batteryLevel);
+    }
 }
 
 @end

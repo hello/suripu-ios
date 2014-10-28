8c3b751c28829a4dc8fe04273df57421c498f238
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-10-28 15:02:32 -0700

    yet another onboarding screen, to scan networks from sense

---
+++ SleepModel/HEMWifiPickerViewController.h
@@ -0,0 +1,14 @@
+//
+//  HEMWifiPickerViewController.h
+//  Sense
+//
+//  Created by Jimmy Lu on 10/27/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "HEMBaseController.h"
+
+@interface HEMWifiPickerViewController : HEMBaseController
+
+@end
---
+++ SleepModel/HEMWifiPickerViewController.m
@@ -0,0 +1,237 @@
+//
+//  HEMWifiPickerViewController.m
+//  Sense
+//
+//  Created by Jimmy Lu on 10/27/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+#import <SenseKit/SENSenseManager.h>
+
+#import "HEMWifiPickerViewController.h"
+#import "HEMBaseController+Protected.h"
+#import "HEMWiFiDataSource.h"
+#import "HelloStyleKit.h"
+#import "HEMOnboardingStoryboard.h"
+#import "HEMWifiPasswordViewController.h"
+#import "HEMActionButton.h"
+
+static CGFloat const kHEMWifiCellHeight = 44.0f;
+static CGFloat const kHEMWifiPickerIconPadding = 5.0f;
+static NSUInteger const kHEMWifiPickerTagLock = 1;
+static NSUInteger const kHEMWifiPickerTagWifi = 2;
+static NSUInteger const kHEMWifiPickerScansRequired = 2;
+
+@interface HEMWifiPickerViewController() <UITableViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *subtitleLabel;
+@property (weak, nonatomic) IBOutlet UITableView *wifiPickerTableView;
+@property (weak, nonatomic) IBOutlet UIButton *helpButton;
+@property (weak, nonatomic) IBOutlet HEMActionButton *scanButton;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *tableViewHeightConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *scanButtonWidthConstraint;
+
+@property (copy, nonatomic)   NSString* selectedSSID;
+@property (strong, nonatomic) HEMWiFiDataSource* wifiDataSource;
+@property (assign, nonatomic, getter=isVisible) BOOL visible;
+
+@end
+
+@implementation HEMWifiPickerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [[self navigationItem] setHidesBackButton:YES];
+    
+    [self setWifiDataSource:[[HEMWiFiDataSource alloc] init]];
+    [[self wifiPickerTableView] setDataSource:[self wifiDataSource]];
+    [[self wifiPickerTableView] setDelegate:self];
+    
+    [[[self scanButton] layer] setBorderWidth:0.0f];
+    
+    [SENAnalytics track:kHEMAnalyticsEventOnBWiFiPass];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self setVisible:YES];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self scanWithActivity];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [self setVisible:NO];
+}
+
+- (void)adjustConstraintsForIphone5 {
+    CGFloat diff = -kHEMWifiCellHeight;
+    [self updateConstraint:[self tableViewHeightConstraint] withDiff:diff];
+}
+
+- (void)adjustConstraintsForIPhone4 {
+    CGFloat diff = -(2*kHEMWifiCellHeight);
+    [self updateConstraint:[self tableViewHeightConstraint] withDiff:diff];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (UIView*)wifiAccessoryView {
+    UIImage* lockIcon = [HelloStyleKit lockIcon];
+    UIImage* wifiIcon = [HelloStyleKit wifiIcon];
+    
+    CGRect lockFrame = CGRectZero;
+    lockFrame.size = lockIcon.size;
+    UIImageView* lockView = [[UIImageView alloc] initWithImage:lockIcon];
+    [lockView setFrame:lockFrame];
+    [lockView setBackgroundColor:[UIColor clearColor]];
+    [lockView setTag:kHEMWifiPickerTagLock];
+    
+    CGRect wifiFrame = CGRectZero;
+    wifiFrame.size = wifiIcon.size;
+    wifiFrame.origin.x = kHEMWifiPickerIconPadding + CGRectGetMaxX(lockFrame);
+    UIImageView* wifiView = [[UIImageView alloc] initWithImage:wifiIcon];
+    [wifiView setFrame:wifiFrame];
+    [wifiView setBackgroundColor:[UIColor clearColor]];
+    [wifiView setTag:kHEMWifiPickerTagWifi];
+    
+    CGRect containerFrame = CGRectZero;
+    containerFrame.size.width = CGRectGetMaxX(wifiFrame);
+    containerFrame.size.height = MAX([lockIcon size].height, [wifiIcon size].height);
+    UIView* container = [[UIView alloc] initWithFrame:containerFrame];
+    container.backgroundColor = [UIColor clearColor];
+
+    [container addSubview:lockView];
+    [container addSubview:wifiView];
+    
+    return container;
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    return kHEMWifiCellHeight;
+}
+
+- (void)tableView:(UITableView *)tableView
+  willDisplayCell:(UITableViewCell *)cell
+forRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UIView* accessoryView = [cell accessoryView];
+    if (accessoryView == nil) {
+        accessoryView = [self wifiAccessoryView];
+        [cell setAccessoryView:accessoryView];
+    }
+    
+    BOOL showWifiIcon = YES;
+    NSString* ssid = [[self wifiDataSource] ssidOfWiFiAtIndexPath:indexPath];
+    if (ssid == nil) {
+        showWifiIcon = NO;
+        ssid = NSLocalizedString(@"settings.wifi.other", nil);
+    }
+    
+    UIImageView* lockView = (UIImageView*)[accessoryView viewWithTag:kHEMWifiPickerTagLock];
+    [lockView setHidden:![[self wifiDataSource] isWiFiSecureAtIndexPath:indexPath]];
+    
+    UIImageView* wifiView = (UIImageView*)[accessoryView viewWithTag:kHEMWifiPickerTagWifi];
+    [wifiView setHidden:!showWifiIcon];
+    
+    [[cell textLabel] setFont:[UIFont fontWithName:@"Calibre-Regular" size:18.0f]];
+    [[cell textLabel] setText:ssid];
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    [self setSelectedSSID:[[self wifiDataSource] ssidOfWiFiAtIndexPath:indexPath]];
+    [self next];
+}
+
+- (void)scanWithActivity {
+    [[self scanButton] showActivityWithWidthConstraint:[self scanButtonWidthConstraint]];
+    
+    __weak typeof(self) weakSelf = self;
+    [self scanUntilDoneWithCount:0 completion:^(NSError *error) {
+        __block typeof(weakSelf) strongSelf = weakSelf;
+        if (strongSelf) {
+            [[strongSelf scanButton] stopActivity];
+            [[strongSelf wifiPickerTableView] reloadData];
+            
+            if (error != nil && [strongSelf isVisible]) {
+                [strongSelf showError:error];
+            }
+        }
+    }];
+}
+
+- (void)scanUntilDoneWithCount:(NSInteger)scanCount completion:(void(^)(NSError* error))completion {
+    __weak typeof(self) weakSelf = self;
+    [[self wifiDataSource] scan:^(NSError *error) {
+        __block typeof(weakSelf) strongSelf = weakSelf;
+        if (strongSelf) {
+            if (error != nil) {
+                if (completion) completion (error);
+                return;
+            }
+            NSInteger nextCount = scanCount + 1;
+            if (nextCount < kHEMWifiPickerScansRequired && [strongSelf isVisible]) {
+                [[strongSelf wifiPickerTableView] reloadData]; // show what we have
+                [strongSelf scanUntilDoneWithCount:nextCount completion:completion];
+            } else {
+                if (completion) completion (nil);
+            }
+        }
+    }];
+}
+
+#pragma mark - Errors / Alerts
+
+- (void)showError:(NSError*)error {
+    NSString* title = NSLocalizedString(@"wifi.error.scan-title", nil);
+    NSString* message = nil;
+    switch ([error code]) {
+        case SENSenseManagerErrorCodeSenseOutOfMemory:
+            message = NSLocalizedString(@"wifi.error.scan-out-of-memory", nil);
+            break;
+        default:
+            message = NSLocalizedString(@"wifi.error.scan-general", nil);
+            break;
+    }
+    [self showMessageDialog:message title:title];
+}
+
+#pragma mark - Actions
+
+- (IBAction)help:(id)sender {
+    DDLogVerbose(@"WARNING: this has not been implemented yet!");
+    
+    [SENAnalytics track:kHEMAnalyticsEventHelp];
+    [self next];
+}
+
+- (IBAction)scan:(id)sender {
+    [SENAnalytics track:kHEMAnalyticsEventOnBWiFiScan];
+    
+    [[self wifiDataSource] clearDetectedWifis];
+    [[self wifiPickerTableView] reloadData];
+    [self scanWithActivity];
+}
+
+#pragma mark - Navigation
+
+- (void)next {
+    [self performSegueWithIdentifier:[HEMOnboardingStoryboard wifiPasswordSegueIdentifier]
+                              sender:nil];
+}
+
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    if ([self selectedSSID] != nil) {
+        UIViewController* destVC = [segue destinationViewController];
+        if ([destVC isKindOfClass:[HEMWifiPasswordViewController class]]) {
+            HEMWifiPasswordViewController* wifiVC = (HEMWifiPasswordViewController*)destVC;
+            [wifiVC setSsid:[self selectedSSID]];
+        }
+    }
+}
+
+@end

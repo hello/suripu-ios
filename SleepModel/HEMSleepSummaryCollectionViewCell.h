3ec0dab30ea29825cb822b16c3ff78872d3ecae3
Delisa Mason <iskanamagus@gmail.com>
2014-10-27 18:59:47 -0700

    Show a wave!

--- SleepModel/HEMSleepSummaryCollectionViewCell.h
+++ SleepModel/HEMSleepSummaryCollectionViewCell.h
@@ -1,10 +1,12 @@
 
 #import <UIKit/UIKit.h>
 
+@class HEMSleepScoreGraphView;
 @interface HEMSleepSummaryCollectionViewCell : UICollectionViewCell
 
 @property (weak, nonatomic) IBOutlet UILabel* dateLabel;
 @property (weak, nonatomic) IBOutlet UILabel* messageLabel;
+@property (weak, nonatomic) IBOutlet HEMSleepScoreGraphView* sleepScoreGraphView;
 
 - (void)setSleepScore:(NSUInteger)sleepScore animated:(BOOL)animated;
 @end

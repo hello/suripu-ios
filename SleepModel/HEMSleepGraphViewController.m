64fc4045329b5fdfb82b909b2fe5390da76dc990
Delisa Mason <iskanamagus@gmail.com>
2014-11-04 16:55:04 -0800

    Fix pre sleep insight item height

--- SleepModel/HEMSleepGraphViewController.m
+++ SleepModel/HEMSleepGraphViewController.m
@@ -37,7 +37,7 @@ static CGFloat const HEMSleepEventPopupFullHeight = 100.f;
 static CGFloat const HEMSleepEventPopupMinimumHeight = 50.f;
 static CGFloat const HEMPresleepHeaderCellHeight = 70.f;
 static CGFloat const HEMTimelineHeaderCellHeight = 50.f;
-static CGFloat const HEMPresleepItemCellHeight = 48.f;
+static CGFloat const HEMPresleepItemCellHeight = 68.f;
 static CGFloat const HEMSleepGraphCollectionViewEventMinimumHeight = 30.f;
 static CGFloat const HEMSleepGraphCollectionViewNumberOfHoursOnscreen = 10.f;
 

0928e67eacef14e35bb74ce8688907c943d66f0f
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-12-01 19:17:01 -0800

    new action view to replace simple info view

---
+++ SleepModel/HEMActionView.h
@@ -0,0 +1,13 @@
+//
+//  HEMActionView.h
+//  Sense
+//
+//  Created by Jimmy Lu on 12/1/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HEMActionView : UIView
+
+@end
---
+++ SleepModel/HEMActionView.m
@@ -0,0 +1,13 @@
+//
+//  HEMActionView.m
+//  Sense
+//
+//  Created by Jimmy Lu on 12/1/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+
+#import "HEMActionView.h"
+
+@implementation HEMActionView
+
+@end

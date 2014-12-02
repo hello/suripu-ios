7b2923277c9f8551e8c50db214b7cb596780ffe9
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-12-02 14:16:10 -0800

    updated sleep question fonts

--- SleepModel/UIFont+HEMStyle.m
+++ SleepModel/UIFont+HEMStyle.m
@@ -104,19 +104,15 @@ static NSString* const HEMLargeNumberFontFamilyName = @"AvenirNext-UltraLight";
 }
 
 + (UIFont *)questionAnswerFont {
-    return [UIFont fontWithName:HEMFontFamilyNameLight size:20.0f];
-}
-
-+ (UIFont *)questionTitleFont {
-    return [UIFont fontWithName:HEMFontFamilyNameLight size:18.0f];
+    return [UIFont fontWithName:HEMFontFamilyNameHeavy size:18.0f];
 }
 
 + (UIFont *)questionFont {
-    return [UIFont fontWithName:HEMFontFamilyNameMedium size:20.0f];
+    return [UIFont fontWithName:HEMFontFamilyNameLight size:24.0f];
 }
 
 + (UIFont *)thankyouFont {
-    return [UIFont fontWithName:HEMFontFamilyNameMedium size:20.0f];
+    return [UIFont fontWithName:HEMFontFamilyNameLight size:24.0f];
 }
 
 + (UIFont *)infoToastFont {

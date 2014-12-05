d4f3bf4216b502c81239364622ad3b6375d214c8
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-12-04 18:14:46 -0800

    reused view to show sensor data during check

--- SleepModel/HEMSensorCheckView.h
+++ SleepModel/HEMSensorCheckView.h
@@ -2,6 +2,9 @@
 //  HEMSensorCheckView.h
 //  Sense
 //
+//  This view displays 1 sensor data as part of the "Room Check" feature, which
+//  is part of Onboarding.
+//
 //  Created by Jimmy Lu on 12/3/14.
 //  Copyright (c) 2014 Hello, Inc. All rights reserved.
 //
@@ -12,9 +15,49 @@ extern CGFloat const HEMSensorCheckCollapsedHeight;
 
 @interface HEMSensorCheckView : UIView
 
+/**
+ * Initialize the view with the icon that represents the sensor, a highlighted
+ * version of the icon, title to display what the sensor is, a message for the
+ * sensor describing it's state and the current value of the sensor along with
+ * it's color of the condition of the sensor.
+ *
+ * @param icon:             icon of sensor
+ * @param highlightedIcon:  highlighted version of the icon
+ * @param title:            title for the sensor
+ * @param message:          attributed message describing state of sensor
+ * @param value:            the value of the sensor
+ * @param color:            condition color of the sensor
+ */
 - (instancetype)initWithIcon:(UIImage*)icon
              highlightedIcon:(UIImage*)highlighedIcon
                        title:(NSString*)title
-                     message:(NSString*)message;
+                     message:(NSAttributedString*)message
+                       value:(NSString*)value
+          withConditionColor:(UIColor*)color;
+
+/**
+ * Move the view to y origin while expanding it's height and applying additional
+ * animations during the process.  This can also be thought of as expanding the
+ * view, which will automatically highlight the sensor icon
+ *
+ * @param y:          the y origin the view should move to
+ * @param height:     the expanded height of the view
+ * @param animations: the animations to additional apply
+ * @param completion: the block to invoke when all is done
+ */
+- (void)moveTo:(CGFloat)y
+   andExpandTo:(CGFloat)height
+whileAnimating:(void(^)(void))animations
+  onCompletion:(void(^)(BOOL finished))completion;
+
+/**
+ * Collapse the view.  This is not animated
+ */
+- (void)collapse;
+
+/**
+ * Show the sensor value that was initialized
+ */
+- (void)showSensorValue;
 
 @end
--- SleepModel/HEMSensorCheckView.m
+++ SleepModel/HEMSensorCheckView.m
@@ -16,12 +16,18 @@ static CGFloat const HEMSensorCheckHorzPadding = 45.0f;
 static CGFloat const HEMSensorCheckLabelTopMargin = 20.0f;
 static CGFloat const HEMSensorCheckTitleHeight = 20.0f;
 static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
+static CGFloat const HEMSensorCheckSeparatorHeight = 0.5f;
+static CGFloat const HEMSensorCheckContentAnimationDuration = 0.7f;
 
 @interface HEMSensorCheckView()
 
-@property (nonatomic, weak) UIImageView* iconImageView;
-@property (nonatomic, weak) UILabel* titleLabel;
-@property (nonatomic, weak) UILabel* messageLabel;
+@property (nonatomic, weak)   UIImageView* iconImageView;
+@property (nonatomic, weak)   UILabel* titleLabel;
+@property (nonatomic, weak)   UILabel* messageLabel;
+@property (nonatomic, weak)   UILabel* valueLabel;
+@property (nonatomic, weak)   UIView* separator;
+@property (nonatomic, copy)   NSString* value;
+@property (nonatomic, strong) UIColor* conditionColor;
 
 @end
 
@@ -34,7 +40,9 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
 - (instancetype)initWithIcon:(UIImage*)icon
              highlightedIcon:(UIImage*)highlighedIcon
                        title:(NSString*)title
-                     message:(NSString*)message {
+                     message:(NSAttributedString*)message
+                       value:(NSString*)value
+          withConditionColor:(UIColor*)color {
     
     CGRect bounds = [[UIScreen mainScreen] bounds];
     CGRect frame = CGRectZero;
@@ -47,6 +55,10 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
         [self addIconImageViewWithIcon:icon highlightedIcon:highlighedIcon];
         [self addTitleWithText:title];
         [self addMessageWithText:message];
+        [self addSeparator];
+        
+        [self setValue:value];
+        [self setConditionColor:color];
     }
     return self;
 }
@@ -55,6 +67,7 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
     UIImageView* imageView = [[UIImageView alloc] initWithImage:icon];
     [imageView setHighlightedImage:highlightedIcon];
     [imageView setContentMode:UIViewContentModeScaleAspectFit];
+    [imageView setAutoresizingMask:UIViewAutoresizingFlexibleBottomMargin];
     
     CGRect imageFrame = [imageView frame];
     imageFrame.size = [icon size];
@@ -63,7 +76,7 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
     [imageView setFrame:imageFrame];
     
     [self addSubview:imageView];
-    [[self iconImageView] setFrame:imageFrame];
+    [self setIconImageView:imageView];
 }
 
 - (void)addTitleWithText:(NSString*)title {
@@ -78,7 +91,6 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
     [label setTextColor:[HelloStyleKit senseBlueColor]];
     [label setText:[title uppercaseString]];
     [label setNumberOfLines:1];
-    [label setHidden:YES];
     [label setAlpha:0.0f];
     [label setTextAlignment:NSTextAlignmentCenter];
     [label setFont:[UIFont onboardingRoomCheckSensorFont]];
@@ -87,15 +99,11 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
     [self setTitleLabel:label];
 }
 
-- (void)addMessageWithText:(NSString*)message {
+- (void)addMessageWithText:(NSAttributedString*)message {
     UILabel* label = [[UILabel alloc] init];
-    [label setText:message];
-    [label setTextColor:[UIColor blackColor]];
-    [label setFont:[UIFont onboardingRoomCheckSensorFont]];
+    [label setAttributedText:message];
     [label setNumberOfLines:3];
-    [label setHidden:YES];
     [label setAlpha:0.0f];
-    [label setTextAlignment:NSTextAlignmentCenter];
     
     CGFloat width = CGRectGetWidth([self bounds]) - (2*HEMSensorCheckHorzPadding);
     CGSize constraint = CGSizeZero;
@@ -112,6 +120,104 @@ static CGFloat const HEMSensorCheckMessageMaxHeight = 58.0f;
     
     [self addSubview:label];
     [self setMessageLabel:label];
+}
+
+- (void)showSensorValue {
+    UILabel* label = [[UILabel alloc] init];
+    [label setText:[self value]];
+    [label setTextColor:[self conditionColor]];
+    [label setFont:[UIFont onboardingRoomCheckSensorValueFont]];
+    [label setNumberOfLines:1];
+    [label sizeToFit];
+    [label setAlpha:0.0f];
+    
+    CGFloat lastY = CGRectGetMaxY([[self messageLabel] frame]);
+    CGFloat spaceLeft = CGRectGetHeight([self bounds]) - lastY;
+    
+    CGRect frame = [label frame];
+    frame.origin.x = (CGRectGetWidth([self bounds]) - CGRectGetWidth(frame))/2;
+    frame.origin.y = lastY + (spaceLeft - CGRectGetHeight(frame))/2;
+    [label setFrame:frame];
+    
+    [self addSubview:label];
+    [self setValueLabel:label];
+    
+    [UIView animateWithDuration:0.7f
+                     animations:^{
+                         [label setAlpha:1.0f];
+                     }];
+}
+
+- (void)addSeparator {
+    CGRect frame = {
+        0.0f,
+        CGRectGetHeight([self bounds]) - HEMSensorCheckSeparatorHeight,
+        CGRectGetWidth([self bounds]),
+        HEMSensorCheckSeparatorHeight
+    };
+    
+    // NOTE: with half pixel on the separator, autolayout
+    // causes the separator to flicker and disppear on iphone 5/5s
+    // so we need to not use constraints and be forced to animate
+    // it ourselves
+    UIView* separator = [[UIView alloc] initWithFrame:frame];
+    [separator setBackgroundColor:[UIColor colorWithWhite:0.0f alpha:0.3f]];
+    [separator setAlpha:0.0f];
+    
+    [self addSubview:separator];
+    [self setSeparator:separator];
+}
+
+- (void)collapse {
+    CGRect frame = [self frame];
+    frame.size.height = HEMSensorCheckCollapsedHeight;
+    [self setFrame:frame];
+    
+    // reset
+    CGRect sepFrame = [[self separator] frame];
+    sepFrame.origin.y = HEMSensorCheckCollapsedHeight - CGRectGetHeight(sepFrame);
+    [[self separator] setFrame:sepFrame];
+    
+    [[self separator] setAlpha:0.0f];
+    [[self titleLabel] setAlpha:0.0f];
+    [[self messageLabel] setAlpha:0.0f];
+}
+
+- (void)moveTo:(CGFloat)y
+   andExpandTo:(CGFloat)height
+whileAnimating:(void(^)(void))animations
+  onCompletion:(void(^)(BOOL finished))completion {
+    
+    [UIView animateWithDuration:HEMSensorCheckContentAnimationDuration
+                          delay:0.0f
+                        options:UIViewAnimationOptionCurveEaseOut
+                     animations:^{
+                         CGRect frame = [self frame];
+                         frame.origin.y = y;
+                         frame.size.height = height;
+                         [self setFrame:frame];
+                         
+                         CGRect sepFrame = [[self separator] frame];
+                         sepFrame.origin.y = height - CGRectGetHeight(sepFrame);
+                         [[self separator] setFrame:sepFrame];
+                         [[self separator] setAlpha:1.0f];
+                         
+                         if (animations) animations();
+                     }
+                     completion:^(BOOL finished) {
+                         [self displayContents:completion];
+                     }];
+    
+}
+
+- (void)displayContents:(void(^)(BOOL finished))completion {
+    [UIView animateWithDuration:HEMSensorCheckContentAnimationDuration
+                     animations:^{
+                         [[self iconImageView] setHighlighted:YES];
+                         [[self titleLabel] setAlpha:1.0f];
+                         [[self messageLabel] setAlpha:1.0f];
+                     }
+                     completion:completion];
 }
 
 @end

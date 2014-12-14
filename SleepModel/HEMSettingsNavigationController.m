7b9f09f9b13c7a14af56e5c14cccf07904b168eb
Delisa Mason <iskanamagus@gmail.com>
2014-12-13 16:18:14 -0800

    Hide/show drawer top bar with child controller state

--- SleepModel/HEMSettingsNavigationController.m
+++ SleepModel/HEMSettingsNavigationController.m
@@ -9,6 +9,8 @@
 #import "UIFont+HEMStyle.h"
 
 #import "HEMSettingsNavigationController.h"
+#import "HEMRootViewController.h"
+#import "HEMAppDelegate.h"
 #import "HEMColorUtils.h"
 #import "HelloStyleKit.h"
 
@@ -31,6 +33,8 @@
     __weak typeof(self) weakSelf = self;
     self.interactivePopGestureRecognizer.delegate = weakSelf;
     self.delegate = weakSelf;
+    [self.interactivePopGestureRecognizer addTarget:self
+                                             action:@selector(interactivePopGestureActivated:)];
 }
 
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated {
@@ -38,11 +42,43 @@
     [self setBackButtonOnViewController:viewController];
     [[viewController view] setBackgroundColor:[HelloStyleKit backViewBackgroundColor]];
     [super pushViewController:viewController animated:animated];
+    [self updateTopBarVisibilityAnimated:animated];
+}
+
+- (UIViewController *)popViewControllerAnimated:(BOOL)animated {
+    UIViewController* controller = [super popViewControllerAnimated:animated];
+    [self updateTopBarVisibilityAnimated:animated];
+    return controller;
+}
+
+- (NSArray *)popToRootViewControllerAnimated:(BOOL)animated {
+    NSArray* controllers = [super popToRootViewControllerAnimated:animated];
+    [self updateTopBarVisibilityAnimated:animated];
+    return controllers;
+}
+
+- (NSArray *)popToViewController:(UIViewController *)viewController animated:(BOOL)animated {
+    NSArray* controllers = [super popToViewController:viewController animated:animated];
+    [self updateTopBarVisibilityAnimated:animated];
+    return controllers;
+}
+
+- (void)navigationController:(UINavigationController *)navigationController
+      willShowViewController:(UIViewController *)viewController
+                    animated:(BOOL)animated {
+    id<UIViewControllerTransitionCoordinator> tc = navigationController.topViewController.transitionCoordinator;
+    [tc notifyWhenInteractionEndsUsingBlock:^(id<UIViewControllerTransitionCoordinatorContext> context) {
+        if ([context isCancelled]) {
+            [self hideTopBarAnimated:animated];
+        } else {
+            [self updateTopBarVisibilityAnimated:animated];
+        }
+    }];
 }
 
 - (void)navigationController:(UINavigationController *)navigationController
        didShowViewController:(UIViewController *)viewController
-                    animated:(BOOL)animate {
+                    animated:(BOOL)animated {
     self.interactivePopGestureRecognizer.enabled = YES;
 }
 
@@ -56,6 +92,42 @@
 
 - (void)goBack {
     [self popViewControllerAnimated:YES];
+}
+
+#pragma mark - Top Bar Handling
+
+- (void)updateBarVisibilityWithRatio:(CGFloat)ratio {
+    HEMAppDelegate* delegate = [UIApplication sharedApplication].delegate;
+    HEMRootViewController* root = (id)delegate.window.rootViewController;
+    [root showPartialSettingsDrawerTopBarWithRatio:ratio];
+}
+
+- (void)updateTopBarVisibilityAnimated:(BOOL)animated {
+    if ([self.topViewController isEqual:[self.viewControllers firstObject]])
+        [self showTopBarAnimated:animated];
+    else
+        [self hideTopBarAnimated:animated];
+}
+
+- (void)hideTopBarAnimated:(BOOL)animated {
+    HEMAppDelegate* delegate = [UIApplication sharedApplication].delegate;
+    HEMRootViewController* root = (id)delegate.window.rootViewController;
+    [root hideSettingsDrawerTopBar:YES animated:animated];
+}
+
+- (void)showTopBarAnimated:(BOOL)animated {
+    HEMAppDelegate* delegate = [UIApplication sharedApplication].delegate;
+    HEMRootViewController* root = (id)delegate.window.rootViewController;
+    [root hideSettingsDrawerTopBar:NO animated:animated];
+}
+
+#pragma mark - Gesture Recognizer
+
+- (void)interactivePopGestureActivated:(UIGestureRecognizer*)recognizer {
+    CGFloat ratio = [recognizer locationInView:self.view].x/CGRectGetWidth(self.view.bounds);
+    if (recognizer.state == UIGestureRecognizerStateChanged) {
+        [self updateBarVisibilityWithRatio:ratio];
+    }
 }
 
 @end

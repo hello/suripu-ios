3904c94650f64264ebfaeefc853feada466e2006
Jimmy Lu <jimmy.m.lu@gmail.com>
2014-12-17 11:06:00 -0800

    new question collection view cell for feed

---
+++ SleepModel/HEMQuestionCell.m
@@ -0,0 +1,47 @@
+//
+//  HEMQuestionCell.m
+//  Sense
+//
+//  Created by Jimmy Lu on 12/15/14.
+//  Copyright (c) 2014 Hello, Inc. All rights reserved.
+//
+
+#import "HEMQuestionCell.h"
+#import "UIFont+HEMStyle.h"
+
+CGFloat const HEMQuestionTextPadding = 22.0f;
+CGFloat const HEMQuestionContentPadding = 8.0f;
+
+@interface HEMQuestionCell()
+
+@property (weak, nonatomic) IBOutlet UILabel* titleLabel;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *dividerWidthConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *questionLeadingConstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *questionTrailingConstraint;
+
+@end
+
+@implementation HEMQuestionCell
+
++ (NSDictionary*)questionaTextAttributes {
+    static NSDictionary* attributes = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSMutableParagraphStyle* style =
+            [[NSMutableParagraphStyle defaultParagraphStyle] mutableCopy];
+        attributes = @{NSFontAttributeName : [UIFont feedQuestionFont],
+                       NSParagraphStyleAttributeName : style};
+    });
+    return attributes;
+}
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    [[self dividerWidthConstraint] setConstant:0.5f];
+    
+    CGFloat padding = HEMQuestionTextPadding-HEMQuestionContentPadding;
+    [[self questionLeadingConstraint] setConstant:padding];
+    [[self questionTrailingConstraint] setConstant:padding];
+}
+
+@end

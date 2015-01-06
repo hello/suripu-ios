3d129dcc75d24f0a14198d2c9b24d410281b6f04
Delisa Mason <iskanamagus@gmail.com>
2015-01-06 10:09:34 -0800

    Remove unused gradient

--- SleepModel/HEMSleepSummaryCollectionViewCell.m
+++ SleepModel/HEMSleepSummaryCollectionViewCell.m
@@ -4,14 +4,7 @@
 #import "HEMSleepScoreGraphView.h"
 #import "HelloStyleKit.h"
 
-@interface HEMSleepSummaryCollectionViewCell ()
-
-@property (nonatomic, strong) CAGradientLayer* gradientLayer;
-@end
-
 @implementation HEMSleepSummaryCollectionViewCell
-
-static CGFloat const HEMSleepSummaryShadowHeight = 1.f;
 
 - (void)awakeFromNib
 {
@@ -30,34 +23,6 @@ static CGFloat const HEMSleepSummaryShadowHeight = 1.f;
 - (void)setSleepScore:(NSUInteger)sleepScore animated:(BOOL)animated
 {
     [self.sleepScoreGraphView setSleepScore:sleepScore animated:animated];
-}
-
-- (void)layoutSubviews
-{
-    [super layoutSubviews];
-    if (!self.gradientLayer) {
-        UIColor* topColor = [UIColor whiteColor];
-        UIColor* bottomColor = [HelloStyleKit timelineGradientDarkColor];
-        self.gradientLayer = [CAGradientLayer layer];
-        self.gradientLayer.colors = @[(id)topColor.CGColor, (id)bottomColor.CGColor];
-        [self.layer insertSublayer:self.gradientLayer atIndex:0];
-    }
-    CGRect gradientRect = self.bounds;
-    gradientRect.size.height -= HEMSleepSummaryShadowHeight;
-    self.gradientLayer.frame = gradientRect;
-}
-
-- (void)drawRect:(CGRect)rect
-{
-    [self drawShadowGradientInRect:rect];
-}
-
-- (void)drawShadowGradientInRect:(CGRect)rect
-{
-    CGContextRef ctx = UIGraphicsGetCurrentContext();
-    CGRect shadowRect = CGRectMake(CGRectGetMinX(rect), CGRectGetHeight(rect) - HEMSleepSummaryShadowHeight, CGRectGetWidth(rect), HEMSleepSummaryShadowHeight);
-    CGContextSetFillColorWithColor(ctx, [HelloStyleKit timelineSectionBorderColor].CGColor);
-    CGContextFillRect(ctx, shadowRect);
 }
 
 @end

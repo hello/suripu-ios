0878a37ca3cd62959ab7edadea768094b8eb54ae
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-01-15 18:14:07 -0800

    set initial state for text field

---
+++ SleepModel/HEMSenseAudioViewController.h
@@ -0,0 +1,13 @@
+//
+//  HEMSenseAudioViewController.h
+//  Sense
+//
+//  Created by Jimmy Lu on 1/15/15.
+//  Copyright (c) 2015 Hello, Inc. All rights reserved.
+//
+
+#import "HEMOnboardingController.h"
+
+@interface HEMSenseAudioViewController : HEMOnboardingController
+
+@end
---
+++ SleepModel/HEMSenseAudioViewController.m
@@ -0,0 +1,13 @@
+//
+//  HEMSenseAudioViewController.m
+//  Sense
+//
+//  Created by Jimmy Lu on 1/15/15.
+//  Copyright (c) 2015 Hello, Inc. All rights reserved.
+//
+
+#import "HEMSenseAudioViewController.h"
+
+@implementation HEMSenseAudioViewController
+
+@end

162a5f93ade119471cb3deb0c2b37dd58a46ba8f
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-01-27 10:54:01 -0800

    always show read more for insight summary in feed

--- SleepModel/HEMInsightCollectionViewCell.m
+++ SleepModel/HEMInsightCollectionViewCell.m
@@ -11,19 +11,12 @@
 #import "HEMInsightCollectionViewCell.h"
 
 CGFloat const HEMInsightCellMessagePadding = 16.0f;
-CGFloat const HEMInsightCellBaseHeight = 88.0f;
-CGFloat const HEMInsightCellMaxMessageHeight = 100.0f;
 
-static CGFloat const HEMInsightCellNaturalPadding = 8.0f;
+static CGFloat const HEMInsightCellBaseHeight = 106.0f;
 
 @interface HEMInsightCollectionViewCell()
 
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint* messageBottomConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint* messageLeadingConstraint;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint* messageTrailingConstraint;
 @property (weak, nonatomic) IBOutlet UILabel* moreLabel;
-
-@property (assign, nonatomic) CGFloat fullMessageBottomConstraintConstant;
 
 @end
 
@@ -52,50 +45,27 @@ static CGFloat const HEMInsightCellNaturalPadding = 8.0f;
     return markdown_to_attr_string(message, 0, markdownAttributes);
 }
 
-+ (CGSize)textSizeForMessage:(NSString*)message inWidth:(CGFloat)contentWidth {
++ (CGFloat)contentHeightWithMessage:(NSString*)message inWidth:(CGFloat)contentWidth {
     NSAttributedString* text = [self attributedTextForMessage:message];
+    
     CGSize constraint = CGSizeMake(contentWidth, MAXFLOAT);
-    NSStringDrawingOptions options = NSStringDrawingUsesFontLeading|NSStringDrawingUsesLineFragmentOrigin;
-    return [text boundingRectWithSize:constraint options:options context:nil].size;
-}
+    NSStringDrawingOptions options =
+        NSStringDrawingUsesFontLeading | NSStringDrawingUsesLineFragmentOrigin;
+    CGSize contentSize = [text boundingRectWithSize:constraint options:options context:nil].size;
 
-- (void)awakeFromNib {
-    [super awakeFromNib];
-    
-    CGFloat padding = HEMInsightCellMessagePadding - HEMInsightCellNaturalPadding;
-    [[self messageLeadingConstraint] setConstant:padding];
-    [[self messageTrailingConstraint] setConstant:padding];
-    
-    [self setFullMessageBottomConstraintConstant:[[self messageBottomConstraint] constant]];
-    
+    return contentSize.height + HEMInsightCellBaseHeight;
 }
 
 - (void)prepareForReuse {
     [super prepareForReuse];
-    
     [[self messageLabel] setAttributedText:nil];
-    [[self moreLabel] setHidden:YES];
-    [[self messageBottomConstraint] setConstant:[self fullMessageBottomConstraintConstant]];
 }
 
 - (void)setMessage:(NSString*)message {
     if ([message length] == 0) return;
-
+    
     NSAttributedString* text = [[self class] attributedTextForMessage:message];
-    CGFloat contentWidth = CGRectGetWidth([[self contentView] bounds])-HEMInsightCellMessagePadding;
-    CGSize textSize = [[self class] textSizeForMessage:message inWidth:contentWidth];
-
     [[self messageLabel] setAttributedText:text];
-    
-    BOOL more = textSize.height > HEMInsightCellMaxMessageHeight;
-    if (more) {
-        [[self moreLabel] setHidden:NO];
-        CGFloat constant = [self fullMessageBottomConstraintConstant];
-        CGFloat moreHeight = CGRectGetHeight([[self moreLabel] bounds]);
-        [[self messageBottomConstraint] setConstant:constant+moreHeight];
-        [self setNeedsDisplay];
-    }
-    
 }
 
 @end

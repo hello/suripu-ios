683672fbcc8fef8162532dc5a08b070eb39e570e
Delisa Mason <iskanamagus@gmail.com>
2015-02-03 17:11:28 -0800

    Add breakdown styling

--- SleepModel/UIFont+HEMStyle.m
+++ SleepModel/UIFont+HEMStyle.m
@@ -116,6 +116,10 @@ static NSString* const HEMNumberFontFamilyNameMedium = @"AvenirNext-Medium";
     return [UIFont fontWithName:HEMFontFamilyNameHeavy size:14.f];
 }
 
++ (UIFont *)timelineBreakdownValueFont {
+    return [UIFont fontWithName:HEMNumberFontFamilyNameUltraLight size:24.f];
+}
+
 + (UIFont *)timelineEventMessageFont {
     return [UIFont fontWithName:HEMFontFamilyNameLight size:13.f];
 }

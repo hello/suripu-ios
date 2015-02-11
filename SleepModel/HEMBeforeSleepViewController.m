7f6d2494a81e64065bd9d392c3b7894691f0d438
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-02-11 11:21:16 -0800

    work in progress on sense colors

--- SleepModel/HEMBeforeSleepViewController.m
+++ SleepModel/HEMBeforeSleepViewController.m
@@ -13,17 +13,22 @@
 #import "HEMBeforeSleepViewController.h"
 #import "HEMBaseController+Protected.h"
 #import "HEMActionButton.h"
-#import "HEMOnboardingUtils.h"
-#import "HEMSupportUtil.h"
 #import "HelloStyleKit.h"
-#import "HEMActivityCoverView.h"
 #import "HEMOnboardingStoryboard.h"
+#import "UIFont+HEMStyle.h"
 
-@interface HEMBeforeSleepViewController()
+static NSInteger const HEMBeforeSleepNumberOfScreens = 5;
+static CGFloat const HEMBeforeSleepTextPadding = 20.0f;
 
-@property (weak, nonatomic) IBOutlet UILabel *subtitleLabel;
+@interface HEMBeforeSleepViewController() <UIScrollViewDelegate>
+
 @property (weak, nonatomic) IBOutlet HEMActionButton *continueButton;
-@property (copy, nonatomic) NSString* nextSegueId;
+@property (weak, nonatomic) IBOutlet UIScrollView *contentScrollView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIPageControl *dots;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *continueButtonBottomConstraint;
+
+@property (assign, nonatomic) CGFloat origContinueButtonBottomConstant;
 
 @end
 
@@ -31,9 +36,70 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    [self enableBackButton:NO];
+    [self configureButtons];
+    [self configureScrollView];
     [SENAnalytics track:kHEMAnalyticsEventOnBSenseColors];
 }
+
+- (void)configureButtons {
+    [self enableBackButton:NO];
+    
+    // hide the continue button initially
+    [self setOrigContinueButtonBottomConstant:[[self continueButtonBottomConstraint] constant]];
+    CGFloat buttonHeight = CGRectGetHeight([[self continueButton] bounds]);
+    [[self continueButtonBottomConstraint] setConstant:-buttonHeight];
+}
+
+- (void)configureScrollView {
+    CGFloat x = HEMBeforeSleepTextPadding;
+    CGFloat contentWidth = CGRectGetWidth([[UIScreen mainScreen] bounds]);
+    NSString* titleKeyFormat = @"onboarding.before-sleep.%ld.title";
+    NSString* titleKey = nil;
+    
+    for (int i = 0; i < HEMBeforeSleepNumberOfScreens; i++) {
+        titleKey = [NSString stringWithFormat:titleKeyFormat, i+1];
+        [self addTitleLabelWithText:NSLocalizedString(titleKey, nil)
+                                 to:[self contentScrollView]
+                                atX:x];
+        x += contentWidth;
+    }
+    
+    CGSize contentSize = [[self contentScrollView] contentSize];
+    contentSize.width = x;
+    [[self contentScrollView] setContentSize:contentSize];
+    [[self dots] setNumberOfPages:HEMBeforeSleepNumberOfScreens];
+    [[self dots] setCurrentPageIndicatorTintColor:[HelloStyleKit senseBlueColor]];
+    [[self dots] setCurrentPage:0];
+}
+
+- (void)addTitleLabelWithText:(NSString*)text to:(UIScrollView*)scrollView atX:(CGFloat)x {
+    CGRect labelFrame = CGRectZero;
+    labelFrame.origin.x = x;
+    
+    UILabel* label = [[UILabel alloc] initWithFrame:labelFrame];
+    [label setBackgroundColor:[scrollView backgroundColor]];
+    [label setText:text];
+    [label setFont:[UIFont onboardingTitleFont]];
+    [label setTextColor:[HelloStyleKit onboardingTitleColor]];
+    [label sizeToFit];
+    
+    [scrollView addSubview:label];
+}
+
+- (void)addSubtitleLabelWithText:(NSAttributedString*)text
+                              to:(UIScrollView*)scrollView
+                             atX:(CGFloat)x {
+    
+}
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    NSInteger screenNumber = [scrollView contentOffset].x / CGRectGetWidth([scrollView bounds]);
+    [[self dots] setCurrentPage:screenNumber];
+}
+
+#pragma mark - Navigation
 
 - (IBAction)next:(id)sender {
     NSString* nextSegueId = [HEMOnboardingStoryboard beforeSleeptoRoomCheckSegueIdentifier];

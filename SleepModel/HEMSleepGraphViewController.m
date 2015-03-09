d64116d7256162b37abbd365b75c7b14dfcdca50
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-03-09 13:59:12 -0700

    fixing merging conflict

--- SleepModel/HEMSleepGraphViewController.m
+++ SleepModel/HEMSleepGraphViewController.m
@@ -343,9 +343,9 @@ static CGFloat const HEMAlarmShortcutHiddenTrailing = -60.f;
 {
     __weak typeof(self) weakSelf = self;
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        [HEMDialogViewController showInfoDialogWithTitle:NSLocalizedString(@"sleep-event.feedback.failed.title", nil)
-                                                 message:NSLocalizedString(@"sleep-event.feedback.failed.message", nil)
-                                              controller:weakSelf];
+        [HEMAlertViewController showInfoDialogWithTitle:NSLocalizedString(@"sleep-event.feedback.failed.title", nil)
+                                                message:NSLocalizedString(@"sleep-event.feedback.failed.message", nil)
+                                             controller:weakSelf];
     });
 }
 

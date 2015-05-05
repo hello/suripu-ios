44224618a4ad3cd75c110a566002ede9791bf052
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-05-05 15:28:57 -0700

    no internet system alert

--- SleepModel/HEMSystemAlertController.m
+++ SleepModel/HEMSystemAlertController.m
@@ -26,11 +26,21 @@
 #import "HEMMainStoryboard.h"
 #import "HEMBounceModalTransition.h"
 
+typedef NS_ENUM(NSUInteger, HEMSystemAlertType) {
+    HEMSystemAlertTypeUnknown = 0,
+    HEMSystemAlertTypeNoInternet = 1,
+    HEMSystemAlertTypeNoTimeZone = 2,
+    HEMSystemAlertTypeNoSensePaired = 3,
+    HEMSystemAlertTypeNoPillPaired = 4,
+    HEMSystemAlertTypeSenseNotSeen = 5,
+    HEMSystemAlertTypePillNotSeen = 6,
+    HEMSystemAlertTypePillLowBattery = 7
+};
+
 @interface HEMSystemAlertController()<HEMPillPairDelegate, HEMSensePairingDelegate>
 
 @property (nonatomic, strong) HEMActionView* alertView;
 @property (nonatomic, weak)   UIViewController* viewController;
-@property (nonatomic, assign) SENServiceDeviceState warningState;
 @property (nonatomic, assign) BOOL enableSystemMonitoring;
 @property (nonatomic, strong) HEMBounceModalTransition* modalTransitionDelegate;
 
@@ -52,6 +62,7 @@
     if (enable) {
         [self checkSystemIfEnabled];
     } else {
+        [self stopListeningForInternetConnectivityChanges];
         [[SENServiceDevice sharedService] resetDeviceStates];
     }
 
@@ -74,7 +85,12 @@
                                                                       attributes:messageAttributes];
     HEMActionView* alert = [[HEMActionView alloc] initWithTitle:title message:attrMessage];
     [[alert cancelButton] setTitle:[cancelTitle uppercaseString] forState:UIControlStateNormal];
-    [[alert okButton] setTitle:[fixTitle uppercaseString] forState:UIControlStateNormal];
+    
+    if (fixTitle) {
+        [[alert okButton] setTitle:[fixTitle uppercaseString] forState:UIControlStateNormal];
+    } else {
+        [alert hideOkButton];
+    }
     
     return alert;
 }
@@ -83,6 +99,74 @@
     [self dismissAlert:nil];
     [SENAnalytics track:HEMAnalyticsEventSystemAlertAction
              properties:@{kHEMAnalyticsEventPropAction : HEMAnalyticsEventSysAlertActionLater}];
+}
+
+#pragma mark - Internet Connectivity Listener
+
+- (void)listenForInternetConnectivityChanges {
+    [self stopListeningForInternetConnectivityChanges];
+    
+    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+    [center addObserver:self
+               selector:@selector(didLoseInternet)
+                   name:SENAPIUnreachableNotification
+                 object:nil];
+    [center addObserver:self
+               selector:@selector(didGetInternetBack)
+                   name:SENAPIReachableNotification
+                 object:nil];
+    [center addObserver:self
+               selector:@selector(checkInternetConnectivity)
+                   name:UIApplicationDidBecomeActiveNotification
+                 object:nil];
+}
+
+- (void)checkInternetConnectivity {
+    if (![SENAPIClient isAPIReachable]) {
+        [self didLoseInternet];
+    } else {
+        [self didGetInternetBack];
+    }
+}
+
+- (void)stopListeningForInternetConnectivityChanges {
+    NSNotificationCenter* center = [NSNotificationCenter defaultCenter];
+    [center removeObserver:self name:SENAPIUnreachableNotification object:nil];
+    [center removeObserver:self name:SENAPIReachableNotification object:nil];
+    [center removeObserver:self name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
+- (void)didLoseInternet {
+    if ([self alertView]) {
+        DDLogVerbose(@"another alert is currently shown, skip showing no internet alert");
+        return;
+    }
+    
+    NSString* title = NSLocalizedString(@"alerts.no-internet.title", nil);
+    NSString* message = NSLocalizedString(@"alerts.no-internet.message", nil);
+    NSString* cancelTitle = NSLocalizedString(@"actions.ok", nil);
+    
+    HEMActionView* alert = [self configureAlertViewWithTitle:title
+                                                     message:message
+                                           cancelButtonTitle:cancelTitle
+                                              fixButtonTitle:nil];
+    
+    [self setAlertView:alert];
+    [[self alertView] setTag:HEMSystemAlertTypeNoInternet];
+    
+    [[[self alertView] cancelButton] addTarget:self
+                                        action:@selector(cancelAlert:)
+                              forControlEvents:UIControlEventTouchUpInside];
+    [[self alertView] showInView:[[self viewController] view] animated:YES completion:nil];
+    
+    [SENAnalytics track:HEMAnalyticsEventSystemAlert
+             properties:@{kHEMAnalyticsEventPropType : @"no internet"}];
+}
+
+- (void)didGetInternetBack {
+    if ([[self alertView] tag] == HEMSystemAlertTypeNoInternet) {
+        [self dismissAlert:nil];
+    }
 }
 
 #pragma mark - Time Zone
@@ -112,6 +196,7 @@
                                            cancelButtonTitle:cancelTitle
                                               fixButtonTitle:fixTitle];
     [self setAlertView:alert];
+    [[self alertView] setTag:HEMSystemAlertTypeNoTimeZone];
     [[[self alertView] cancelButton] addTarget:self
                                         action:@selector(cancelAlert:)
                               forControlEvents:UIControlEventTouchUpInside];
@@ -138,8 +223,27 @@
 
 #pragma mark - Devices
 
+- (NSString*)analyticsPropertyTypeValueForDeviceState:(SENServiceDeviceState)state {
+    switch (state) {
+        case SENServiceDeviceStatePillLowBattery:
+            return @"pill has low battery";
+        case SENServiceDeviceStatePillNotPaired:
+            return @"pill is not paired";
+        case SENServiceDeviceStatePillNotSeen:
+            return @"pill has not been seen for awhile";
+        case SENServiceDeviceStateSenseNotSeen:
+            return @"sense has not been seen for awhile";
+        case SENServiceDeviceStateSenseNotPaired:
+            return @"sense is not paired";
+        default:
+            return @"unknown";
+    }
+}
+
 - (void)checkSystemIfEnabled {
     if ([self enableSystemMonitoring]) {
+        [self listenForInternetConnectivityChanges];
+        
         __weak typeof(self) weakSelf = self;
         [[SENServiceDevice sharedService] checkDevicesState:^(SENServiceDeviceState state) {
             __strong typeof(weakSelf) strongSelf = weakSelf;
@@ -164,15 +268,25 @@
     NSString* message = nil;
     NSString* cancelTitle = nil;
     NSString* fixTitle = nil;
+    HEMSystemAlertType alertType = HEMSystemAlertTypeUnknown;
     
     SENServiceDevice* service = [SENServiceDevice sharedService];
-    [self setWarningState:[service deviceState]];
-    [self deviceWarningTitle:&title message:&message cancelButtonTitle:&cancelTitle fixButtonTitle:&fixTitle];
+    [self deviceWarningTitle:&title
+                     message:&message
+           cancelButtonTitle:&cancelTitle
+              fixButtonTitle:&fixTitle
+                   alertType:&alertType];
     
-    if ( message != nil) { // title is optional, but if message its nil, then we assume warning is not supported
-        [self showDeviceWarningWithTitle:title message:message cancelButtonTitle:cancelTitle fixButtonTitle:fixTitle];
+    if ( alertType != HEMSystemAlertTypeUnknown ) {
+        [self showDeviceWarningWithTitle:title
+                                 message:message
+                       cancelButtonTitle:cancelTitle
+                          fixButtonTitle:fixTitle
+                                 forType:alertType];
+        
+        NSString* analyticsType = [self analyticsPropertyTypeValueForDeviceState:[service deviceState]];
         [SENAnalytics track:HEMAnalyticsEventSystemAlert
-                 properties:@{kHEMAnalyticsEventPropType : @([service deviceState])}];
+                 properties:@{kHEMAnalyticsEventPropType : analyticsType}];
     }
     
 }
@@ -180,34 +294,42 @@
 - (void)deviceWarningTitle:(NSString**)warningTitle
                    message:(NSString**)warningMessage
          cancelButtonTitle:(NSString**)cancelTitle
-            fixButtonTitle:(NSString**)fixTitle {
+            fixButtonTitle:(NSString**)fixTitle
+                 alertType:(HEMSystemAlertType*)type {
     
-    switch ([self warningState]) {
+    SENServiceDevice* service = [SENServiceDevice sharedService];
+    switch ([service deviceState]) {
         case SENServiceDeviceStateSenseNotPaired:
             *warningTitle = NSLocalizedString(@"alerts.device.no-sense.title", nil);
             *warningMessage = NSLocalizedString(@"alerts.device.no-sense.message", nil);
+            *type = HEMSystemAlertTypeNoSensePaired;
             break;
         case SENServiceDeviceStateSenseNotSeen:
             *warningTitle = NSLocalizedString(@"alerts.device.sense-last-seen.title", nil);
             *warningMessage = NSLocalizedString(@"alerts.device.sense-last-seen.message", nil);
+            *type = HEMSystemAlertTypeSenseNotSeen;
             break;
         case SENServiceDeviceStatePillNotPaired:
             *warningTitle = NSLocalizedString(@"alerts.device.no-pill.title", nil);
             *warningMessage = NSLocalizedString(@"alerts.device.no-pill.message", nil);
+            *type = HEMSystemAlertTypeNoPillPaired;
             break;
         case SENServiceDeviceStatePillNotSeen:
             *warningTitle = NSLocalizedString(@"alerts.device.pill-last-seen.title", nil);
             *warningMessage = NSLocalizedString(@"alerts.device.pill-last-seen.message", nil);
+            *type = HEMSystemAlertTypePillNotSeen;
             break;
         case SENServiceDeviceStatePillLowBattery:
             *warningMessage = NSLocalizedString(@"alerts.device.pill-low-battery.message", nil);
             *cancelTitle = NSLocalizedString(@"actions.skip", nil);
             *fixTitle = NSLocalizedString(@"actions.replace", nil);
+            *type = HEMSystemAlertTypePillLowBattery;
         default:
+            *type = HEMSystemAlertTypeUnknown;
             break;
     }
     
-    if (*warningMessage != nil) {
+    if (*type != HEMSystemAlertTypeUnknown) {
         if (*cancelTitle == nil) {
             *cancelTitle = NSLocalizedString(@"actions.later", nil);
         }
@@ -219,14 +341,18 @@
 
 }
 
-- (void)showDeviceWarningWithTitle:(NSString*)title message:(NSString*)message
-                 cancelButtonTitle:(NSString*)cancelTitle fixButtonTitle:(NSString*)fixTitle {
+- (void)showDeviceWarningWithTitle:(NSString*)title
+                           message:(NSString*)message
+                 cancelButtonTitle:(NSString*)cancelTitle
+                    fixButtonTitle:(NSString*)fixTitle
+                           forType:(HEMSystemAlertType)type {
     
     HEMActionView* alert = [self configureAlertViewWithTitle:title
                                                      message:message
                                            cancelButtonTitle:cancelTitle
                                               fixButtonTitle:fixTitle];
     [self setAlertView:alert];
+    [[self alertView] setTag:type];
     [[[self alertView] cancelButton] addTarget:self
                                         action:@selector(fixDeviceProblemLater:)
                               forControlEvents:UIControlEventTouchUpInside];
@@ -237,44 +363,41 @@
 }
 
 - (void)fixDeviceProblemLater:(id)sender {
-    [self dismissAlert:^{
-        [self setWarningState:SENServiceDeviceStateUnknown];
-    }];
+    [self dismissAlert:nil];
     [SENAnalytics track:HEMAnalyticsEventSystemAlertAction
              properties:@{kHEMAnalyticsEventPropAction : HEMAnalyticsEventSysAlertActionLater}];
 }
 
 - (void)fixDeviceProblemNow:(id)sender {
+    HEMSystemAlertType alertType = [[self alertView] tag];
     [self dismissAlert:^{
-        [self launchHandlerForDeviceState];
+        [self launchHandlerForDeviceAlertType:alertType];
     }];
     [SENAnalytics track:HEMAnalyticsEventSystemAlertAction
              properties:@{kHEMAnalyticsEventPropAction : HEMAnalyticsEventSysAlertActionNow}];
 }
 
-- (void)launchHandlerForDeviceState {
+- (void)launchHandlerForDeviceAlertType:(HEMSystemAlertType)type {
     // supported warnings are handled below
-    switch ([self warningState]) {
-        case SENServiceDeviceStateSenseNotPaired:
+    switch (type) {
+        case HEMSystemAlertTypeNoSensePaired:
             [self showSensePairController];
             break;
-        case SENServiceDeviceStateSenseNotSeen:
+        case HEMSystemAlertTypeSenseNotSeen:
             [self showSenseHelp];
             break;
-        case SENServiceDeviceStatePillNotPaired:
+        case HEMSystemAlertTypeNoPillPaired:
             [self showPillPairController];
             break;
-        case SENServiceDeviceStatePillNotSeen:
+        case HEMSystemAlertTypePillNotSeen:
             [self showPillHelp];
             break;
-        case SENServiceDeviceStatePillLowBattery:
+        case HEMSystemAlertTypePillLowBattery:
             [self showHowToReplacePillBattery];
             break;
         default:
             break;
     }
-    // reset it
-    [self setWarningState:SENServiceDeviceStateUnknown];
 }
 
 - (void)dismissAlert:(void(^)(void))completion {
@@ -304,12 +427,10 @@
 }
 
 - (void)didUpdatePairing:(NSNotification*)notification {
-    switch ([self warningState]) {
-        case SENServiceDeviceStateSenseNotPaired:
-        case SENServiceDeviceStatePillNotPaired:
-            if ([self alertView]) {
-                [self dismissAlert:nil];
-            }
+    switch ([[self alertView] tag]) {
+        case HEMSystemAlertTypeNoSensePaired:
+        case HEMSystemAlertTypeNoPillPaired:
+            [self dismissAlert:nil];
             break;
         default:
             break;

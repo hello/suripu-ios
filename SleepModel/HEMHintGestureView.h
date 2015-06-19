21cda8082283906920e374ec73d63a29fc15201a
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-06-19 10:24:44 -0700

    add comments

--- SleepModel/HEMHintGestureView.h
+++ SleepModel/HEMHintGestureView.h
@@ -10,8 +10,24 @@
 
 @interface HEMHintGestureView : UIView
 
+/**
+ * Initialize an instance of the gesture hint.  This is intended to be used with
+ * HEMHandholdingView.
+ *
+ * @param frame: the frame for the gesture.  Frame determines the starting center
+ * @param endCenter: the center point for which this view will end at when the
+ *                   animation ends
+ */
 - (instancetype)initWithFrame:(CGRect)frame withEndCenter:(CGPoint)endCenter;
+
+/**
+ * Begin auto-reversed animation of the gesture
+ */
 - (void)startAnimation;
+
+/**
+ * Stop the animation
+ */
 - (void)endAnimation;
 
 @end
--- SleepModel/HEMHintMessageView.h
+++ SleepModel/HEMHintMessageView.h
@@ -12,6 +12,13 @@
 
 @property (nonatomic, strong, readonly) UIButton* dismissButton;
 
+/**
+ * Initialize the instance with a message to be displayed for the hint.  This
+ * is meant to be used within HEMHandholding in junction with HEMHintGestureView
+ *
+ * @param message: the message to display
+ * @param width: the full width of the container
+ */
 - (instancetype)initWithMessage:(NSString*)message constrainedToWidth:(CGFloat)width;
 
 @end

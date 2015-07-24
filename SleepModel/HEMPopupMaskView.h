5374ae5aba1c90544875866df86823ee0a6a1e65
Delisa Mason <iskanamagus@gmail.com>
2015-07-24 15:59:04 -0700

    Add mask view handling cutout

---
+++ SleepModel/HEMPopupMaskView.h
@@ -0,0 +1,14 @@
+//
+//  HEMPopupMaskView.h
+//  Sense
+//
+//  Created by Delisa Mason on 7/24/15.
+//  Copyright (c) 2015 Hello. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HEMPopupMaskView : UIView
+
+- (void)showUnderlyingViewRect:(CGRect)rect;
+@end

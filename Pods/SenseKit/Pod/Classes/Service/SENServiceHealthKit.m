c1af1e07063ed65453ce449b3e2a12da279373ef
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-08-11 13:09:39 -0700

    updating pods to pull in sensekit changes for hk

--- Pods/SenseKit/Pod/Classes/Service/SENServiceHealthKit.m
+++ Pods/SenseKit/Pod/Classes/Service/SENServiceHealthKit.m
@@ -21,7 +21,8 @@
 
 static NSString* const SENServiceHKErrorDomain = @"is.hello.service.hk";
 static NSString* const SENServiceHKLastDateWritten = @"is.hello.service.hk.lastdate";
-static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
+static NSString* const SENServiceHKEnable = @"is.hello.service.hk.enable";
+static CGFloat const SENServiceHKBackFillLimit = 3;
 
 @interface SENServiceHealthKit()
 
@@ -58,7 +59,32 @@ static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
     }
 }
 
-#pragma mark -
+#pragma mark - Preferences / Settings
+
+- (void)setEnableHealthKit:(BOOL)enable {
+    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
+    [preferences setUserPreference:@(enable) forKey:SENServiceHKEnable];
+}
+
+- (BOOL)isHealthKitEnabled {
+    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
+    return [[preferences userPreferenceForKey:SENServiceHKEnable] boolValue];
+}
+
+- (void)saveLastWrittenDate:(NSDate*)date {
+    if (date == nil) {
+        return;
+    }
+    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
+    [preferences setUserPreference:date forKey:SENServiceHKLastDateWritten];
+}
+
+- (NSDate*)lastWrittenDate {
+    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
+    return [preferences userPreferenceForKey:SENServiceHKLastDateWritten];
+}
+
+#pragma mark - Support / Authorization
 
 - (BOOL)isSupported {
     return [self hkStore] != nil;
@@ -69,18 +95,6 @@ static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
     HKCategoryType* hkSleepCategory = [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis];
     HKAuthorizationStatus status = [[self hkStore] authorizationStatusForType:hkSleepCategory];
     return status == HKAuthorizationStatusSharingAuthorized;
-}
-
-- (BOOL)addedDataPointFor:(NSDate*)date {
-    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
-    NSDate* lastWrittenDate = [preferences userPreferenceForKey:SENServiceHKLastDateWritten];
-    return [lastWrittenDate isEqualToDate:date];
-}
-
-- (void)saveLastWrittenDate:(NSDate*)date {
-    if (date == nil) return;
-    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
-    [preferences setUserPreference:date forKey:SENServiceHKLastDateWritten];
 }
 
 - (void)requestAuthorization:(void(^)(NSError* error))completion {
@@ -94,6 +108,7 @@ static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
     }
     
     HKCategoryType* hkSleepCategory = [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis];
+    
     NSSet* writeTypes = [NSSet setWithObject:hkSleepCategory];
     NSSet* readTypes = [NSSet setWithObject:hkSleepCategory]; // there will be more, soon
     
@@ -124,6 +139,8 @@ static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
     }];
 }
 
+#pragma mark - Sync
+
 - (void)sync:(void(^)(NSError* error))completion {
     void(^done)(NSError* error) = ^(NSError* error) {
         if (completion) {
@@ -134,16 +151,9 @@ static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
     BOOL enabled = [self isHealthKitEnabled];
     BOOL supported = [self isSupported];
     BOOL authorized = [self canWriteSleepAnalysis];
+    
     if (enabled && supported && authorized) {
-        NSDate* lastNight = [self lastNight];
-        if (![self addedDataPointFor:lastNight]) {
-            [self writeSleepAnalysisIfDataAvailableFor:lastNight completion:completion];
-            return;
-        } else {
-            done ([NSError errorWithDomain:SENServiceHKErrorDomain
-                                      code:SENServiceHealthKitErrorAlreadySynced
-                                  userInfo:nil]);
-        }
+        [self syncRecentMissingDays:done];
     } else {
         SENServiceHealthKitError code;
         if (!enabled) {
@@ -157,143 +167,179 @@ static NSString* const SENSErviceHKEnable = @"is.hello.service.hk.enable";
     }
 }
 
-/**
- * @return NSDate without time to represent the previous day
- */
-- (NSDate*)lastNight {
+- (void)syncRecentMissingDays:(void(^)(NSError* error))completion {
     NSCalendar* calendar = [[NSCalendar alloc] initWithCalendarIdentifier:NSCalendarIdentifierGregorian];
     
-    NSCalendarUnit unitsWeWant = NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit;
-    NSDateComponents* todayComponents = [calendar components:unitsWeWant fromDate:[NSDate date]];
+    // last night
+    NSCalendarUnit unitsWeCareAbout = NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit;
+    NSDateComponents* todayComponents = [calendar components:unitsWeCareAbout fromDate:[NSDate date]];
     NSDate* today = [calendar dateFromComponents:todayComponents];
     
     NSDateComponents* lastNightComponents = [[NSDateComponents alloc] init];
     [lastNightComponents setDay:-1];
-    return [calendar dateByAddingComponents:lastNightComponents toDate:today options:0];
+    NSDate* lastNight = [calendar dateByAddingComponents:lastNightComponents toDate:today options:0];
+    
+    // last time it was sync'ed
+    NSDate* syncStartDate = [self lastWrittenDate];
+    
+    if (syncStartDate) {
+        NSDateComponents *difference = [calendar components:NSCalendarUnitDay
+                                                   fromDate:syncStartDate
+                                                     toDate:lastNight
+                                                    options:0];
+        if ([difference day] == 0) {
+            completion ([NSError errorWithDomain:SENServiceHKErrorDomain
+                                            code:SENServiceHealthKitErrorAlreadySynced
+                                        userInfo:nil]);
+            return;
+        } else if ([difference day] > SENServiceHKBackFillLimit) { // make sure we don't backfill too much
+            NSDateComponents* backFillComps = [[NSDateComponents alloc] init];
+            [backFillComps setDay:-SENServiceHKBackFillLimit];
+            syncStartDate = [calendar dateByAddingComponents:backFillComps toDate:lastNight options:0];
+        }
+    } else { // if never been sync'ed before, just sync last night's data
+        syncStartDate = lastNight;
+    }
+    
+    __weak typeof(self) weakSelf = self;
+    [self syncTimelineDataAfter:syncStartDate until:lastNight withCalendar:calendar completion:^(NSError *error) {
+        if (!error) {
+            [weakSelf saveLastWrittenDate:lastNight];
+        }
+        completion (error);
+    }];
 }
 
-- (void)writeSleepAnalysisIfDataAvailableFor:(NSDate*)date completion:(void(^)(NSError* error))completion {
-    SENTimeline* result = [SENTimeline timelineForDate:date];
-
-    if ([[result segments] count] > 0) {
-        DDLogVerbose(@"adding sleep data point to health kit for date %@", [result date]);
-        [self writeSleepDataPoints:result forDate:date completion:completion];
-    } else {
-        DDLogVerbose(@"pulling from server since no data is in the cache");
-        [SENAPITimeline timelineForDate:date completion:^(NSArray* timelines, NSError* error) {
-            if (error == nil) {
-                SENTimeline* freshResult = nil;
-                if ([timelines isKindOfClass:[NSArray class]] && [timelines count] > 0) {
-                    freshResult = [[SENTimeline alloc] initWithDictionary:[timelines firstObject]];
-                }
-                
-                if ([[freshResult segments] count] > 0) {
-                    DDLogVerbose(@"adding sleep data point to HealthKit for date %@", [freshResult date]);
-                    [freshResult save];
-                    [self writeSleepDataPoints:freshResult forDate:date completion:completion];
-                } else {
-                    if (completion) {
-                        completion ([NSError errorWithDomain:SENServiceHKErrorDomain
-                                                        code:SENServiceHealthKitErrorNoDataToWrite
-                                                    userInfo:nil]);
-                    }
-                }
-            } else if (completion) {
-                completion (error);
+- (void)syncTimelineDataAfter:(NSDate*)startDate
+                        until:(NSDate*)endDate
+                 withCalendar:(NSCalendar*)calendar
+                   completion:(void(^)(NSError* error))completion {
+    NSCalendarUnit unitsWeCareAbout = NSYearCalendarUnit|NSMonthCalendarUnit|NSDayCalendarUnit;
+    NSDate* nextStartDate = startDate;
+    NSUInteger daysFromStartDate = 0;
+    NSDateComponents* components = nil;
+    
+    NSMutableArray* timelines = [NSMutableArray array];
+    dispatch_group_t getTimelineGroup = dispatch_group_create();
+    
+    __weak typeof(self) weakSelf = self;
+    while ([calendar compareDate:nextStartDate toDate:endDate toUnitGranularity:unitsWeCareAbout] != NSOrderedDescending) {
+        __strong typeof(weakSelf) strongSelf = weakSelf;
+        
+        dispatch_group_enter(getTimelineGroup);
+        [strongSelf timelineForDate:nextStartDate completion:^(SENTimeline *timeline, NSError *error) {
+            if (timeline) {
+                [timelines addObject:timeline];
             }
+            dispatch_group_leave(getTimelineGroup);
+        }];
+        
+        components = [calendar components:unitsWeCareAbout fromDate:nextStartDate];
+        [components setDay:daysFromStartDate];
+        nextStartDate = [calendar dateByAddingComponents:components toDate:nextStartDate options:0];
+        daysFromStartDate++;
+    }
+    
+    long queuePriority = DISPATCH_QUEUE_PRIORITY_DEFAULT;
+    dispatch_queue_t queue = dispatch_get_global_queue(queuePriority, 0);
+    dispatch_group_notify(getTimelineGroup, queue, ^{
+        [weakSelf syncTimelinesToHealthKit:timelines completion:^(NSError *error) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                completion (error);
+            });
+        }];
+    });
+
+}
+
+- (void)timelineForDate:(NSDate*)date completion:(void(^)(SENTimeline* timeline, NSError* error))completion {
+    SENTimeline* timeline = [SENTimeline timelineForDate:date];
+    if ([[timeline metrics] count] > 0 && [timeline scoreCondition] != SENConditionUnknown) {
+        completion (timeline, nil);
+    } else {
+        [SENAPITimeline timelineForDate:date completion:^(id data, NSError *error) {
+            SENTimeline* timeline = data;
+            if (!error) {
+                if ([timeline isKindOfClass:[SENTimeline class]]) {
+                    [timeline save];
+                } else {
+                    timeline = nil;
+                    error = [NSError errorWithDomain:SENServiceHKErrorDomain
+                                                code:SENServiceHealthKitErrorUnexpectedAPIResponse
+                                            userInfo:nil];
+                }
+            }
+            completion (timeline, error);
         }];
     }
 }
 
-- (NSArray*)sleepDataPointsForSleepResult:(SENTimeline*)sleepResult {
-    NSMutableArray* dataPoints = [NSMutableArray arrayWithCapacity:1];
+- (void)syncTimelinesToHealthKit:(NSArray*)timelines completion:(void(^)(NSError* error))completion {
+    NSUInteger timelineCount = [timelines count];
+    if (timelineCount == 0) {
+        completion ([NSError errorWithDomain:SENServiceHKErrorDomain
+                                        code:SENServiceHealthKitErrorNoDataToWrite
+                                    userInfo:nil]);
+        return;
+    }
+    
+    HKSample* sample = nil;
+    NSMutableArray* samples = [NSMutableArray arrayWithCapacity:timelineCount];
+    for (SENTimeline* timeline in timelines) {
+        sample = [self sleepSampleFromTimeline:timeline];
+        if (sample) {
+            [samples addObject:sample];
+        }
+    }
+    
+    if ([samples count] == 0) {
+        if (completion) {
+            completion ([NSError errorWithDomain:SENServiceHKErrorDomain
+                                            code:SENServiceHealthKitErrorNoDataToWrite
+                                        userInfo:nil]);
+        }
+        return;
+    }
+    
+    [[self hkStore] saveObjects:samples withCompletion:^(BOOL success, NSError *error) {
+        completion (error);
+    }];
+}
+
+- (HKSample*)sleepSampleFromTimeline:(SENTimeline*)sleepResult {
+    HKSample* sample = nil;
     HKCategoryType* hkSleepCategory = [HKObjectType categoryTypeForIdentifier:HKCategoryTypeIdentifierSleepAnalysis];
     NSDate* wakeUpDate = nil;
     NSDate* sleepDate = nil;
-    NSArray* segments = [sleepResult segments];
+    NSArray* metrics = [sleepResult metrics];
 
-    for (SENTimelineSegment* segment in segments) {
-        if (!sleepDate && segment.type == SENTimelineSegmentTypeFellAsleep) {
-            sleepDate = [segment date];
+    for (SENTimelineMetric* metric in metrics) {
+        if (!sleepDate
+            && metric.type == SENTimelineMetricTypeFellAsleep
+            && metric.unit == SENTimelineMetricUnitTimestamp) {
+            sleepDate = [NSDate dateWithTimeIntervalSince1970:[metric.value doubleValue] / 1000];
         }
 
-        if (sleepDate != nil && segment.type == SENTimelineSegmentTypeWokeUp) {
-            wakeUpDate = [segment date];
+        if (sleepDate != nil
+            && metric.type == SENTimelineMetricTypeWokeUp
+            && metric.unit == SENTimelineMetricUnitTimestamp) {
+            wakeUpDate = [NSDate dateWithTimeIntervalSince1970:[metric.value doubleValue] / 1000];
         }
     }
     
     if (wakeUpDate != nil && sleepDate != nil) {
         DDLogVerbose(@"adding asleep data point");
         if ([wakeUpDate compare:sleepDate] > NSOrderedAscending) {
-            [dataPoints addObject:[HKCategorySample categorySampleWithType:hkSleepCategory
-                                                                     value:HKCategoryValueSleepAnalysisAsleep
-                                                                 startDate:sleepDate
-                                                                   endDate:wakeUpDate]];
+            sample = [HKCategorySample categorySampleWithType:hkSleepCategory
+                                                        value:HKCategoryValueSleepAnalysisAsleep
+                                                    startDate:sleepDate
+                                                      endDate:wakeUpDate];
         } else {
             DDLogVerbose(@"wake up time %@ is before sleep time %@", wakeUpDate, sleepDate);
         }
     }
     
-    return dataPoints;
-}
-
-- (void)writeSleepDataPoints:(SENTimeline*)sleepReult
-                     forDate:(NSDate*)date
-                  completion:(void(^)(NSError* error))competion {
-    
-    if (![self isSupported]) {
-        if (competion) {
-            competion ([NSError errorWithDomain:SENServiceHKErrorDomain
-                                           code:SENServiceHealthKitErrorNotSupported
-                                       userInfo:nil]);
-        }
-        return;
-    }
-    
-    void(^finishOnMainThread)(NSError* error) = ^(NSError* error) {
-        if (competion) {
-            dispatch_async(dispatch_get_main_queue(), ^{
-                competion (error);
-            });
-        }
-    };
-    
-    __weak typeof(self) weakSelf = self;
-    long queuePriority = DISPATCH_QUEUE_PRIORITY_DEFAULT;
-    dispatch_async(dispatch_get_global_queue(queuePriority, 0), ^{
-        __strong typeof(weakSelf) strongSelf = weakSelf;
-        NSArray* sleepObjects = [strongSelf sleepDataPointsForSleepResult:sleepReult];
-        
-        if ([sleepObjects count] == 0) {
-            finishOnMainThread ([NSError errorWithDomain:SENServiceHKErrorDomain
-                                                    code:SENServiceHealthKitErrorNoDataToWrite
-                                                userInfo:nil]);
-            return;
-        }
-        
-        [[strongSelf hkStore] saveObjects:sleepObjects withCompletion:^(BOOL success, NSError *error) {
-            
-            if (success) {
-                DDLogVerbose(@"saved sleep result to health kit.");
-                [strongSelf saveLastWrittenDate:date];
-            } else {
-                DDLogVerbose(@"failed to save sleep result to health kit with error %@", error);
-            }
-            
-            finishOnMainThread (error);
-        }];
-    });
-
-}
-
-- (void)setEnableHealthKit:(BOOL)enable {
-    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
-    [preferences setUserPreference:@(enable) forKey:SENSErviceHKEnable];
-}
-
-- (BOOL)isHealthKitEnabled {
-    SENLocalPreferences* preferences = [SENLocalPreferences sharedPreferences];
-    return [[preferences userPreferenceForKey:SENSErviceHKEnable] boolValue];
+    return sample;
 }
 
 @end

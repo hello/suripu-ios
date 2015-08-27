1eccc3b949bdbda55ca1e717c68ac7d1bc2810d7
Delisa Mason <iskanamagus@gmail.com>
2015-08-27 15:18:35 -0700

    Add toggling the drawer as the magic tap gesture

--- SleepModel/HEMRootViewController.m
+++ SleepModel/HEMRootViewController.m
@@ -137,6 +137,11 @@ static CGFloat const HEMRootDrawerStatusBarOffset = 20.f;
     }
 }
 
+- (BOOL)accessibilityPerformMagicTap {
+    [self toggleSettingsDrawer];
+    return YES;
+}
+
 - (void)viewDidEnterBackground
 {
     [super viewDidEnterBackground];
@@ -363,12 +368,10 @@ static CGFloat const HEMRootDrawerStatusBarOffset = 20.f;
 {
     switch (paneState) {
     case MSDynamicsDrawerPaneStateClosed:
-        UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, NSLocalizedString(@"drawer.action.close", nil));
         [[NSNotificationCenter defaultCenter] postNotificationName:HEMRootDrawerMayCloseNotification
                                                             object:nil];
         break;
     case MSDynamicsDrawerPaneStateOpen:
-        UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, NSLocalizedString(@"drawer.action.open", nil));
         [[NSNotificationCenter defaultCenter] postNotificationName:HEMRootDrawerMayOpenNotification
                                                             object:nil];
         break;
@@ -383,11 +386,13 @@ static CGFloat const HEMRootDrawerStatusBarOffset = 20.f;
 {
     switch (paneState) {
     case MSDynamicsDrawerPaneStateClosed:
+            UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, NSLocalizedString(@"drawer.action.close", nil));
         [[NSNotificationCenter defaultCenter] postNotificationName:HEMRootDrawerDidCloseNotification
                                                             object:nil];
         [SENAnalytics track:kHEMAnalyticsEventTimelineClose];
         break;
     case MSDynamicsDrawerPaneStateOpen:
+            UIAccessibilityPostNotification(UIAccessibilityScreenChangedNotification, NSLocalizedString(@"drawer.action.open", nil));
         [[NSNotificationCenter defaultCenter] postNotificationName:HEMRootDrawerDidOpenNotification
                                                             object:nil];
         [SENAnalytics track:kHEMAnalyticsEventTimelineOpen];

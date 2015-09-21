4d636851cbc09121682fcab67b55cdea3cae3cd3
Delisa Mason <iskanamagus@gmail.com>
2015-09-21 16:10:47 -0700

    Upgrade to cocoapods 0.38.2 - Deduplicate dependencies

---
+++ Pods/Headers/Public/ZendeskSDK/ZDKHelpCenterSessionCache.h
@@ -0,0 +1,1 @@
+../../../ZendeskSDK/ZendeskSDK.framework/Headers/ZDKHelpCenterSessionCache.h
--- Pods/Target Support Files/NAPickerView/NAPickerView-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NAPickerView : NSObject
-@end
-@implementation PodsDummy_NAPickerView
-@end

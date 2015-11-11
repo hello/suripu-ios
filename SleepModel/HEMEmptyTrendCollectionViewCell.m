e52a9fcb6bf73039e44ab669704a092cf0eb515e
Jimmy Lu <jimmy.m.lu@gmail.com>
2015-11-11 11:27:28 -0800

    fix spacing issue with empty trends state view

--- SleepModel/HEMEmptyTrendCollectionViewCell.m
+++ SleepModel/HEMEmptyTrendCollectionViewCell.m
@@ -8,11 +8,24 @@
 
 #import "UIFont+HEMStyle.h"
 #import "UIColor+HEMStyle.h"
+#import "NSString+HEMUtils.h"
 
 #import "HEMEmptyTrendCollectionViewCell.h"
 
+static CGFloat const HEMEmptyTrendImageHeight = 180.0f;
+static CGFloat const HEMEmptyTrendHorzMargin = 40.0f;
+static CGFloat const HEMEmptyTrendVertMargin = 24.0f;
 
 @implementation HEMEmptyTrendCollectionViewCell
+
++ (CGFloat)heightWithDescription:(NSString*)description cellWidth:(CGFloat)width {
+    NSDictionary* attrs = @{NSFontAttributeName : [UIFont emptyStateDescriptionFont],
+                           NSForegroundColorAttributeName : [UIColor emptyStateDescriptionColor]};
+    
+    CGFloat actualWidth = width - (HEMEmptyTrendHorzMargin * 2);
+    CGFloat textHeight = [description heightBoundedByWidth:actualWidth attributes:attrs];
+    return HEMEmptyTrendImageHeight + textHeight + (HEMEmptyTrendVertMargin * 2);
+}
 
 - (void)awakeFromNib {
     [super awakeFromNib];

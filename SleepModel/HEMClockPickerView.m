4d846143e55302286736543d639f9789336e44ce
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-01-08 17:37:24 -0800

    fix warnings and issue with iOS 8

--- SleepModel/HEMClockPickerView.m
+++ SleepModel/HEMClockPickerView.m
@@ -44,7 +44,6 @@ static CGFloat const HEMClockPickerUnderlayHeight = 96.0f;
 
 static NSUInteger const HEMClockMinuteCount = 60;
 static NSUInteger const HEMClock12HourCount = 12;
-static NSUInteger const HEMClock24HourCount = 24;
 
 - (instancetype)initWithFrame:(CGRect)frame {
     if (self = [super initWithFrame:frame]) {

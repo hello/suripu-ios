3ad11fc3afec7503976df0e92e662f507a37baa7
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-01-22 13:45:35 -0800

    better handling when no connection

---
+++ SleepModel/HEMSwitchControl.m
@@ -0,0 +1,22 @@
+//
+//  HEMSwitchControl.m
+//  Sense
+//
+//  Originally created by Delisa Mason for alarms.  Still needed
+//
+//  Created by Jimmy Lu on 1/22/16.
+//  Copyright © 2016 Hello. All rights reserved.
+//
+
+#import "HEMSwitchControl.h"
+#import "HEMStyle.h"
+
+@implementation HEMSwitchControl
+
+- (void)awakeFromNib {
+    self.onTintColor = [UIColor tintColor];
+    self.backgroundColor = [UIColor switchOffBackgroundColor];
+    self.layer.cornerRadius = ceilf(CGRectGetHeight(self.bounds) / 2);
+}
+
+@end

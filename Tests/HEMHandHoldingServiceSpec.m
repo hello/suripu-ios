9cd69912e6e3020d5d4105cfc612960d1ddd9719
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-01-27 14:16:58 -0800

    add tests for handholding service

--- Tests/HEMHandHoldingServiceSpec.m
+++ Tests/HEMHandHoldingServiceSpec.m
@@ -11,10 +11,24 @@
 #import <SenseKit/Model.h>
 #import <SenseKit/SENLocalPreferences.h>
 #import "HEMHandHoldingService.h"
+#import "HEMAppUsage.h"
+#import "NSDate+HEMRelative.h"
 
 @interface HEMHandHoldingService()
 
 @property (nonatomic, strong) NSMutableDictionary* tutorialRecordKeeper;
+
+- (BOOL)shouldShowInsightTap;
+- (BOOL)shouldShowTimelineSwipe;
+- (BOOL)shouldShowTimelineZoom;
+- (BOOL)isFirstAppUsage:(NSString*)usageName atLeast:(NSInteger)days;
+- (BOOL)isComplete:(HEMHandHolding)tutorial;
+
+@end
+
+@interface HEMAppUsage()
+
+@property (nonatomic, strong) NSDate* created;
 
 @end
 
@@ -90,6 +104,223 @@ describe(@"HEMHandHoldingService", ^{
             
             it(@"should not have set the records in local prefs again", ^{
                 [[@(savedRecords) should] beNo];
+            });
+            
+        });
+        
+    });
+    
+    describe(@"-isFirstAppUsage:atLeast:", ^{
+        
+        context(@"never seen the app usage stat before", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                
+                HEMAppUsage* fakeUsage = [HEMAppUsage new];
+                [HEMAppUsage stub:@selector(appUsageForIdentifier:) andReturn:fakeUsage];
+            });
+            
+            afterEach(^{
+                [HEMAppUsage clearStubs];
+            });
+            
+            it(@"should return YES if 0 days passed in", ^{
+                BOOL result = [service isFirstAppUsage:@"test" atLeast:0];
+                [[@(result) should] beYes];
+            });
+            
+            it(@"should return NO if 1 day passed in", ^{
+                BOOL result = [service isFirstAppUsage:@"test" atLeast:1];
+                [[@(result) should] beNo];
+            });
+            
+        });
+        
+        context(@"app usage stat created yesterday", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            __block HEMAppUsage* fakeUsage = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                
+                fakeUsage = [HEMAppUsage new];
+                [fakeUsage setCreated:[[NSDate date] previousDay]];
+                [HEMAppUsage stub:@selector(appUsageForIdentifier:) andReturn:fakeUsage];
+            });
+            
+            afterEach(^{
+                fakeUsage = nil;
+                service = nil;
+                [HEMAppUsage clearStubs];
+            });
+            
+            it(@"should return YES if 0 days passed in", ^{
+                BOOL result = [service isFirstAppUsage:@"test" atLeast:0];
+                [[@(result) should] beYes];
+            });
+            
+            it(@"should return YES if 1 day passed in", ^{
+                BOOL result = [service isFirstAppUsage:@"test" atLeast:1];
+                [[@(result) should] beYes];
+            });
+            
+        });
+        
+    });
+    
+    describe(@"-shouldShowTimelineSwipe", ^{
+        
+        context(@"the open timeline tutorial has not been completed, but meets reqs", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                [service stub:@selector(isComplete:) andReturn:[KWValue valueWithBool:NO]];
+                [service stub:@selector(isFirstAppUsage:atLeast:) andReturn:[KWValue valueWithBool:YES]];
+            });
+            
+            afterEach(^{
+                [service clearStubs];
+                service = nil;
+            });
+            
+            it(@"should return NO, even if it meets requirements", ^{
+                BOOL show = [service shouldShowTimelineSwipe];
+                [[@(show) should] beNo];
+            });
+            
+        });
+        
+        context(@"the open timeline tutorial has been completed and meets reqs", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                [service stub:@selector(isComplete:) andReturn:[KWValue valueWithBool:YES]];
+                [service stub:@selector(isFirstAppUsage:atLeast:) andReturn:[KWValue valueWithBool:YES]];
+            });
+            
+            afterEach(^{
+                [service clearStubs];
+                service = nil;
+            });
+            
+            it(@"should return YES", ^{
+                BOOL show = [service shouldShowTimelineSwipe];
+                [[@(show) should] beYes];
+            });
+            
+        });
+        
+        context(@"the open timeline tutorial has been completed, but does not meet reqs", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                [service stub:@selector(isComplete:) andReturn:[KWValue valueWithBool:YES]];
+                [service stub:@selector(isFirstAppUsage:atLeast:) andReturn:[KWValue valueWithBool:NO]];
+            });
+            
+            afterEach(^{
+                [service clearStubs];
+                service = nil;
+            });
+            
+            it(@"should return NO", ^{
+                BOOL show = [service shouldShowTimelineSwipe];
+                [[@(show) should] beNo];
+            });
+            
+        });
+        
+    });
+    
+    describe(@"-shouldShowTimelineZoom", ^{
+        
+        context(@"timeline open and swipe tutorial has not been completed", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                [service stub:@selector(isComplete:) andReturn:[KWValue valueWithBool:NO]];
+            });
+            
+            afterEach(^{
+                [service clearStubs];
+                service = nil;
+            });
+            
+            it(@"should return NO", ^{
+                BOOL show = [service shouldShowTimelineZoom];
+                [[@(show) should] beNo];
+            });
+            
+        });
+        
+        context(@"pre-requites met, but not enough timelines shown yet", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            __block HEMAppUsage* fakeUsage = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                [service stub:@selector(isComplete:) andReturn:[KWValue valueWithBool:YES]];
+                
+                fakeUsage = [HEMAppUsage new];
+                [fakeUsage stub:@selector(usageWithin:) andReturn:[KWValue valueWithInteger:1]];
+                
+                [HEMAppUsage stub:@selector(appUsageForIdentifier:) andReturn:fakeUsage];
+            });
+            
+            afterEach(^{
+                [fakeUsage clearStubs];
+                [service clearStubs];
+                service = nil;
+                fakeUsage = nil;
+                [HEMAppUsage clearStubs];
+            });
+            
+            it(@"should return NO", ^{
+                BOOL show = [service shouldShowTimelineZoom];
+                [[@(show) should] beNo];
+            });
+            
+        });
+        
+        context(@"pre-requites met and have seen tons of timeslines", ^{
+            
+            __block HEMHandHoldingService* service = nil;
+            __block HEMAppUsage* fakeUsage = nil;
+            
+            beforeEach(^{
+                service = [HEMHandHoldingService new];
+                [service stub:@selector(isComplete:) andReturn:[KWValue valueWithBool:YES]];
+                
+                fakeUsage = [HEMAppUsage new];
+                [fakeUsage stub:@selector(usageWithin:) andReturn:[KWValue valueWithInteger:1000]];
+                
+                [HEMAppUsage stub:@selector(appUsageForIdentifier:) andReturn:fakeUsage];
+            });
+            
+            afterEach(^{
+                [fakeUsage clearStubs];
+                [service clearStubs];
+                service = nil;
+                fakeUsage = nil;
+                [HEMAppUsage clearStubs];
+            });
+            
+            it(@"should return YES", ^{
+                BOOL show = [service shouldShowTimelineZoom];
+                [[@(show) should] beYes];
             });
             
         });

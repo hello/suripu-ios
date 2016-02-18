c48df81ab1877c994e782cb8a56e5cdd7c14c5d9
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-02-18 11:44:09 -0800

    fix bug that causes multiple questions to be asked after skipping

--- SleepModel/HEMInsightsFeedPresenter.m
+++ SleepModel/HEMInsightsFeedPresenter.m
@@ -389,10 +389,13 @@ static CGFloat const HEMInsightsFeedImageParallaxMultipler = 2.0f;
 #pragma mark - Actions
 
 - (BOOL)removeQuestionFromData:(nonnull SENQuestion*)question {
+    DDLogVerbose(@"removing question with id %@", [question questionId]);
     NSMutableArray* mutableData = [[self data] mutableCopy];
+    DDLogVerbose(@"data before removal %ld", [mutableData count]);
     NSInteger dataCount = [mutableData count];
     [mutableData removeObject:question];
     [self setData:mutableData];
+    DDLogVerbose(@"data after removal %ld", [[self data] count]);
     
     NSMutableArray* mutableQuestions = [[self questions] mutableCopy];
     [mutableQuestions removeObject:question];
@@ -416,22 +419,7 @@ static CGFloat const HEMInsightsFeedImageParallaxMultipler = 2.0f;
     if (question) {
         // optimistically skip the question
         [self removeQuestion:question atIndexPath:path];
-        
-        __weak typeof(self) weakSelf = self;
-        [[self questionsService] skipQuestion:question completion:^(NSArray<SENQuestion *> * _Nullable questions, NSError * _Nullable error) {
-            __strong typeof(weakSelf) strongSelf = weakSelf;
-            
-            if (!error) {
-                [strongSelf setQuestions:questions];
-                if ([questions count] > 0) {
-                    NSMutableArray* mutableData = [[strongSelf data] mutableCopy];
-                    [mutableData insertObject:questions[0] atIndex:0];
-                    [strongSelf setData:mutableData];
-                    [[strongSelf collectionView] reloadData];
-                }
-            }
-
-        }];
+        [[self questionsService] skipQuestion:question completion:nil];
     }
 }
 

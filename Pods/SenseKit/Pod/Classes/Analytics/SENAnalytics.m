7de8c4f4718b9ce89b6e35f9cb3364923cd8d851
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-03-17 15:18:45 -0700

    pick up sensekit updates

--- Pods/SenseKit/Pod/Classes/Analytics/SENAnalytics.m
+++ Pods/SenseKit/Pod/Classes/Analytics/SENAnalytics.m
@@ -70,6 +70,9 @@ static NSMutableDictionary* providers;
         [mutableProps setValue:@([error code]) forKey:kSENAnalyticsPropCode];
         [mutableProps setValue:[error localizedDescription] forKey:kSENAnalyticsPropMessage];
         [mutableProps setValue:[error domain] forKey:kSENAnalyticsPropDomain];
+        if ([[error userInfo] count] > 0) {
+            [mutableProps addEntriesFromDictionary:[error userInfo]];
+        }
     }
     [providers enumerateKeysAndObjectsUsingBlock:^(NSNumber* key, id<SENAnalyticsProvider> provider, BOOL *stop) {
         [provider track:eventName withProperties:mutableProps];

8a834c2524eb52b2df407157b96a5b84e4875b5d
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-03-17 15:35:48 -0700

    update sensekit, again

--- Pods/SenseKit/Pod/Classes/Analytics/SENAnalytics.m
+++ Pods/SenseKit/Pod/Classes/Analytics/SENAnalytics.m
@@ -13,6 +13,7 @@ NSString* const kSENAnalyticsPropConnection = @"connection";
 NSString* const kSENAnalyticsPropCode = @"code";
 NSString* const kSENAnalyticsPropMessage = @"message";
 NSString* const kSENAnalyticsPropDomain = @"domain";
+NSString* const kSENAnalyticsPropUrl = @"url";
 
 @implementation SENAnalytics
 
@@ -70,8 +71,9 @@ static NSMutableDictionary* providers;
         [mutableProps setValue:@([error code]) forKey:kSENAnalyticsPropCode];
         [mutableProps setValue:[error localizedDescription] forKey:kSENAnalyticsPropMessage];
         [mutableProps setValue:[error domain] forKey:kSENAnalyticsPropDomain];
-        if ([[error userInfo] count] > 0) {
-            [mutableProps addEntriesFromDictionary:[error userInfo]];
+        if ([[error domain] isEqualToString:NSURLErrorDomain] && [error userInfo]) {
+            NSString* url = [error userInfo][NSURLErrorFailingURLStringErrorKey];
+            [mutableProps setValue:url forKey:kSENAnalyticsPropUrl];
         }
     }
     [providers enumerateKeysAndObjectsUsingBlock:^(NSNumber* key, id<SENAnalyticsProvider> provider, BOOL *stop) {

d3f64f42a76e2b5e3a11ce5abd09210cb0a3916e
Jimmy Lu <jimmy.m.lu@gmail.com>
2016-06-24 13:17:29 -0700

    remove system macros

--- SleepModel/HEMGlobalContants.h
+++ SleepModel/HEMGlobalContants.h
@@ -11,9 +11,3 @@
 #import "SENAnalytics+HEMAppAnalytics.h"
 
 extern NSInteger ddLogLevel;
-
-#define SYSTEM_VERSION_EQUAL_TO(v)                  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedSame)
-#define SYSTEM_VERSION_GREATER_THAN(v)              ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedDescending)
-#define SYSTEM_VERSION_GREATER_THAN_OR_EQUAL_TO(v)  ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN(v)                 ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] == NSOrderedAscending)
-#define SYSTEM_VERSION_LESS_THAN_OR_EQUAL_TO(v)     ([[[UIDevice currentDevice] systemVersion] compare:v options:NSNumericSearch] != NSOrderedDescending)

c710a33d0b42f4b48a4fa10b4873713294664b53
Jimmy Lu <jimmy.m.lu@gmail.com>
2017-01-31 15:38:41 -0800

    removed unused line of code

--- SleepModel/Date+Format.swift
+++ SleepModel/Date+Format.swift
@@ -11,8 +11,7 @@ import Foundation
 extension Date {
     
     fileprivate static let isoDateFormat = "yyyy-MM-dd"
-    fileprivate static let timeZoneUTC = "UTC"
-    
+
     static func from(isoDateOnly: String?) -> Date? {
         guard let isoDate = isoDateOnly else {
             return nil

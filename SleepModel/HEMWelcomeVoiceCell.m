dd5fd0414af92883c2c6fc38e020ad044d886fb2
Jimmy Lu <jimmy.m.lu@gmail.com>
2017-03-22 16:16:33 -0700

    themed voice feed

--- SleepModel/HEMWelcomeVoiceCell.m
+++ SleepModel/HEMWelcomeVoiceCell.m
@@ -6,6 +6,7 @@
 //  Copyright © 2016 Hello. All rights reserved.
 //
 
+#import "Sense-Swift.h"
 #import "HEMWelcomeVoiceCell.h"
 #import "NSString+HEMUtils.h"
 
@@ -20,6 +21,22 @@ static CGFloat kHEMWelcomeVoiceTextMargin = 20.0f;
     CGFloat labelWidth = cellWidth - (kHEMWelcomeVoiceTextMargin * 2.0f);
     CGFloat height = [message heightBoundedByWidth:labelWidth usingFont:font];
     return kHEMWelcomeVoiceBaseCellHeight + height;
+}
+
+- (void)applyStyle {
+    [super applyStyle];
+    
+    UIColor* color = [SenseStyle colorWithAClass:[self class] property:ThemePropertyTextColor];
+    UIColor* messageColor = [SenseStyle colorWithAClass:[self class] property:ThemePropertyDetailColor];
+    UIFont* font = [SenseStyle fontWithAClass:[self class] property:ThemePropertyTextFont];
+    UIFont* messageFont = [SenseStyle fontWithAClass:[self class] property:ThemePropertyDetailFont];
+    UIColor* separatorColor = [SenseStyle colorWithAClass:[self class] property:ThemePropertySeparatorColor];
+    [[self titleLabel] setFont:font];
+    [[self titleLabel] setTextColor:color];
+    [[self messageLabel] setFont:messageFont];
+    [[self messageLabel] setTextColor:messageColor];
+    [[self separator] setBackgroundColor:separatorColor];
+    [[self closeButton] applySecondaryStyle];
 }
 
 @end
